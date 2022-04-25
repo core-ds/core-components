cf50ab1313eed9a46b457a26a9f42d7ac644dddd
Alex Yatsenko <reme3d2y@gmail.com>
2022-04-25 23:20:58 +0700

    fix: correct cssm build

--- tools/rollup/ignore-css.js
+++ tools/rollup/ignore-css.js
@@ -1,9 +1,33 @@
+/**
+ * Игнорирует импорты css-модулей
+ * import styles from './index.module.css'; —> var styles = require("./index.module.css");
+ */
 export default function ignoreCss() {
     return {
         name: 'ignore-css',
-        resolveId(source) {
-            if (source.includes('.css')) {
-                return false;
+        transform(_, id) {
+            if (id.includes('.module.css')) {
+                // Достаем путь до импорта и добавляем require
+                const { source } = this.getModuleInfo(id).meta;
+
+                return `export default require("${source}")`;
+            }
+
+            return null;
+        },
+        async resolveId(source, importer, options) {
+            if (source.includes('.module.css')) {
+                // Сначала резолвим импорт css-модуля
+                const resolution = await this.resolve(source, importer, {
+                    skipSelf: true,
+                    ...options,
+                });
+
+                return {
+                    id: resolution.id,
+                    // Запоминаем путь оригинального файла
+                    meta: { source },
+                };
             }
 
             return null;
