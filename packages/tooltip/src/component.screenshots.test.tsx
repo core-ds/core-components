9f9f3eb69db3e4550d2bf53eb713c2d7196ec7a3
Valeri8888 <87258678+Valeri8888@users.noreply.github.com>
2022-05-20 19:18:04 +0300

    feat(tooltip): add inverted colors (#2)

--- packages/tooltip/src/component.screenshots.test.tsx
+++ packages/tooltip/src/component.screenshots.test.tsx
@@ -61,7 +61,7 @@ describe('Tooltip', () => {
         await closeBrowser({ browser, context, page });
     });
 
-    test('test hint view', async () => {
+    test('test hint view and colors', async () => {
         jest.setTimeout(120000);
 
         const cases = generateTestCases({
@@ -72,6 +72,49 @@ describe('Tooltip', () => {
                 onOpenDelay: 0,
                 position: 'top',
                 view: 'hint',
+                colors: ['default', 'inverted'],
+            },
+        });
+
+        const { browser, context, page, css } = await openBrowserPage(cases[0][1]);
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        for (const [_, pageUrl] of cases) {
+            try {
+                await page.goto(pageUrl);
+
+                await page.hover('*[class^=target]');
+
+                await matchHtml({
+                    page,
+                    expect,
+                    css,
+                    matchImageSnapshotOptions: {
+                        failureThresholdType: 'percent',
+                        failureThreshold: 1.5,
+                    },
+                });
+            } catch (error) {
+                // eslint-disable-next-line no-console
+                await console.error(error.message);
+            }
+        }
+
+        await closeBrowser({ browser, context, page });
+    });
+
+    test('test tooltip view and colors', async () => {
+        jest.setTimeout(120000);
+
+        const cases = generateTestCases({
+            componentName: 'Tooltip',
+            testStory: false,
+            knobs: {
+                open: true,
+                onOpenDelay: 0,
+                position: 'top',
+                view: 'tooltip',
+                colors: ['default', 'inverted'],
             },
         });
 
