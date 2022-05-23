9302dff3ef2283ca3fae4dce8411a9464ca3467b
Vladimir <ikus58@gmail.com>
2022-05-23 15:27:12 +0700

    fix(*): fixed launch storybook in IE

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,17 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const cssModuleRegex = /\.module\.css$/;
 const cssRegex = /\.css$/;
+const storiesRegex = /(stories|story)\.mdx$/;
+
+const isIeMode = process.env.STORYBOOK_IE_MODE === 'true';
+
+const babelPresetEnvOptions = {
+    shippedProposals: true,
+    modules: false,
+    targets: 'defaults',
+};
+
+const babelPresetEnvPreset = require.resolve('@babel/preset-env');
 
 const addPackagesDir = config => {
     config.module.rules.forEach(rule => {
@@ -12,10 +23,71 @@ const addPackagesDir = config => {
             rule.oneOf.forEach(nestedRule => {
                 if (nestedRule.loader && nestedRule.loader.includes('babel-loader')) {
                     nestedRule.include.push(path.resolve(__dirname, '../packages'));
+
+                    if (isIeMode && nestedRule.options && nestedRule.options.presets) {
+                        nestedRule.options.sourceType = 'unambiguous';
+                        nestedRule.options.presets.unshift([
+                            babelPresetEnvPreset,
+                            { ...babelPresetEnvOptions, loose: true },
+                        ]);
+                    }
                 }
             });
         }
     });
+};
+
+const addPresetEnvToMdxLoader = config => {
+    config.module.rules.forEach(rule => {
+        if (String(rule.test) === String(storiesRegex)) {
+            if (rule.use) {
+                rule.use.forEach(nestedRule => {
+                    if (nestedRule.loader && nestedRule.loader.includes('babel-loader')) {
+                        nestedRule.options.presets.unshift([
+                            babelPresetEnvPreset,
+                            babelPresetEnvOptions,
+                        ]);
+                    }
+                });
+            }
+        }
+    });
+};
+
+const es6Transpiler = () => {
+    const nodeModulesThatNeedToBeParsedBecauseTheyExposeES6 = [
+        'acorn-jsx',
+        'regexpu-core',
+        'd3',
+        'internmap',
+        'slash',
+        'prettier',
+        'unicode-match-property-ecmascript',
+        'unicode-match-property-value-ecmascript',
+        'strip-ansi',
+        'react-github-btn',
+    ].map(n => new RegExp(`[\\\\/]node_modules[\\\\/]${n}`));
+
+    const include = input => {
+        return !!nodeModulesThatNeedToBeParsedBecauseTheyExposeES6.find(p => input.match(p));
+    };
+
+    return {
+        test: /\.js$/,
+        use: [
+            {
+                loader: require.resolve('babel-loader'),
+                options: {
+                    sourceType: 'unambiguous',
+                    presets: [
+                        [babelPresetEnvPreset, babelPresetEnvOptions],
+                        require.resolve('@babel/preset-react'),
+                    ],
+                },
+            },
+        ],
+        include,
+    };
 };
 
 module.exports = {
@@ -97,6 +169,11 @@ module.exports = {
             ],
         };
 
+        if (isIeMode) {
+            config.module.rules.push(es6Transpiler());
+            addPresetEnvToMdxLoader(config);
+        }
+
         config.plugins.push(
             new MiniCssExtractPlugin(),
             new OptimizeCSSAssetsPlugin({
@@ -108,7 +185,12 @@ module.exports = {
                 },
                 cssProcessorPluginOptions: {
                     preset: () => ({
-                        plugins: [require('postcss-discard-duplicates')],
+                        plugins: [
+                            require('postcss-discard-duplicates'),
+                            ...(isIeMode
+                                ? [require('postcss-custom-properties').bind({ preserve: false })]
+                                : []),
+                        ],
                     }),
                 },
             }),
