431ddb53cfc3e95d721ca802aab705b94845968d
Vladimir Gevak <VGevak@alfabank.ru>
2022-06-03 09:49:00 +0700

    chore: echo debug data

--- bin/publish-ci.sh
+++ bin/publish-ci.sh
@@ -7,28 +7,28 @@ echo "$NPM_TOKEN"
 
 echo "$(<.npmrc )"
 
-# Релизим рут-пакет с помощью semantic-release
-semantic_output=$(yarn semantic-release --debug)
-
-# Выводим логи семантика
-echo "$semantic_output"
-
-# Проверяем, что semantic-release зарелизил рут-пакет (пока не знаю, как это можно сделать по-другому)
-if [[ $semantic_output =~ "Publishing version" ]]
-then
-    git remote set-url origin https://core-ds-bot:"$GITHUB_TOKEN"@github.com/core-ds/core-components.git
-    git checkout master
-    git pull origin master --rebase
-    git fetch --tags
-
-    if [ -z "$(lerna changed)" ]
-    then
-        echo "There are no relevant changes, so no new versions are released."
-    else
-        lerna version --conventional-commits --no-commit-hooks --yes --force-git-tag
-        git push origin master
-        lerna publish from-git --yes
-    fi
-else
-    exit 0
-fi
+## Релизим рут-пакет с помощью semantic-release
+#semantic_output=$(yarn semantic-release --debug)
+#
+## Выводим логи семантика
+#echo "$semantic_output"
+#
+## Проверяем, что semantic-release зарелизил рут-пакет (пока не знаю, как это можно сделать по-другому)
+#if [[ $semantic_output =~ "Publishing version" ]]
+#then
+#    git remote set-url origin https://core-ds-bot:"$GITHUB_TOKEN"@github.com/core-ds/core-components.git
+#    git checkout master
+#    git pull origin master --rebase
+#    git fetch --tags
+#
+#    if [ -z "$(lerna changed)" ]
+#    then
+#        echo "There are no relevant changes, so no new versions are released."
+#    else
+#        lerna version --conventional-commits --no-commit-hooks --yes --force-git-tag
+#        git push origin master
+#        lerna publish from-git --yes
+#    fi
+#else
+#    exit 0
+#fi
