f88b420c63ba6bc3ca5fe826c8e4d45e62a8035c
Vladimir Gevak <VGevak@alfabank.ru>
2022-06-03 09:49:00 +0700

    chore: echo debug data

--- bin/publish-ci.sh
+++ bin/publish-ci.sh
@@ -2,21 +2,26 @@
 
 set -e
 
+echo "$GITHUB_TOKEN"
+echo "$NPM_TOKEN"
+
+echo "$(<.npmrc )"
+
 # Релизим рут-пакет с помощью semantic-release
 semantic_output=$(yarn semantic-release --debug)
 
 # Выводим логи семантика
-echo $semantic_output
+echo "$semantic_output"
 
-# Проеряем, что semantic-release зарелизил рут-пакет (пока не знаю, как это можно сделать по-другому)
+# Проверяем, что semantic-release зарелизил рут-пакет (пока не знаю, как это можно сделать по-другому)
 if [[ $semantic_output =~ "Publishing version" ]]
 then
-    git remote set-url origin https://core-ds-bot:$GITHUB_TOKEN@github.com/core-ds/core-components.git
+    git remote set-url origin https://core-ds-bot:"$GITHUB_TOKEN"@github.com/core-ds/core-components.git
     git checkout master
     git pull origin master --rebase
     git fetch --tags
 
-    if [ -z $(lerna changed) ]
+    if [ -z "$(lerna changed)" ]
     then
         echo "There are no relevant changes, so no new versions are released."
     else
