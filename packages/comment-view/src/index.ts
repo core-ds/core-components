29b2936fa18e306f7d3b0f4a780b1e096898d6de
EgorNey <ENey@alfabank.ru>
2022-07-06 17:26:04 +0300

    feat(comment-view): add comment view component

---
+++ packages/comment-view/src/index.ts
@@ -0,0 +1,1 @@
+export * from './Сomponent';
---
+++ packages/comment-view/src/Сomponent.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import cn from 'classnames';
+
+import { Typography } from '@alfalab/core-components-typography';
+
+import styles from './index.module.css';
+
+export type CommentViewProps = {
+  /** Сss класс для стилизации общей обёртки */
+  className?: string;
+  /** Id компонента для тестов */
+  dataTestId?: string;
+};
+
+export const CommentView: React.FC<CommentViewProps> = ({
+  className,
+  dataTestId,
+  children,
+}) => (
+  <div className={cn(styles.component, className)} data-test-id={dataTestId}>
+    <Typography.Text tag="div" view="component" className={styles.text} color="primary">
+      {children}
+    </Typography.Text>
+  </div>
+);
