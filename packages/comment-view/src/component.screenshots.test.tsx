7ba074359613a7099231003f24eb63628a4067f7
EgorNey <ENey@alfabank.ru>
2022-07-07 21:06:41 +0300

    feat(comment-view): add screenshot testing

---
+++ packages/comment-view/src/component.screenshots.test.tsx
@@ -0,0 +1,30 @@
+import {
+  setupScreenshotTesting,
+  createSpriteStorybookUrl,
+} from '../../screenshot-utils';
+
+const screenshotTesting = setupScreenshotTesting({
+  it,
+  beforeAll,
+  afterAll,
+  expect,
+});
+
+
+describe(
+  'Comment view | main prop',
+  screenshotTesting({
+    cases: [
+      [
+        'sprite',
+        createSpriteStorybookUrl({
+          componentName: 'CommentView',
+          size: { width: 150, height: 40 },
+          knobs: {
+            children: 'Comment',
+          },
+        }),
+      ],
+    ]
+  }),
+);
