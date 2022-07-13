83df58eb2de22d4a646fc291a4cf3abe5d2d5e15
Vladimir Gevak <VGevak@alfabank.ru>
2022-07-13 16:36:54 +0700

    chore: changed packages publishing directory

--- rollup.config.js
+++ rollup.config.js
@@ -93,6 +93,7 @@ const es5 = {
         copy({
             targets: [{ src: ['../../bin/send-stats.js'], dest: 'dist' }],
         }),
+        copy({ targets: [{ src: ['package.json', 'CHANGELOG.md'], dest: 'dist' }] }),
         compiledDarkmodeGenerator(`${currentPackageDir}/dist`),
     ],
 };
