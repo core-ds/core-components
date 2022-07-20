afffd18f3a6cdf363cbc95cf0513ff1bbc63d0f7
Vladimir Gevak <VGevak@alfabank.ru>
2022-07-20 19:30:19 +0700

    feat(codemod): codemode to remove dist from imports

---
+++ packages/codemod/src/delete-dist/transform.ts
@@ -0,0 +1,33 @@
+const transformPaths = (fileInfo, api, options) => {
+    const j = api.jscodeshift;
+    const source = j(fileInfo.source);
+
+    const packages = options.packages ? options.packages.split(',') : [];
+
+    const importsForReplace = source
+        .find(j.ImportSpecifier)
+        .filter(path => {
+            const importFrom = path.parent.value.source.value;
+
+            if (packages.length > 0) {
+                const reList = packages.map(p => new RegExp(`^@alfalab/core-components-${p}/dist`));
+
+                return reList.some(re => re.test(importFrom));
+            }
+
+            const re = new RegExp('^(@alfalab/core-components-).+(/dist)');
+            return re.test(importFrom);
+        })
+        .map(p => p.parent);
+
+    importsForReplace.replaceWith(p => {
+        return j.importDeclaration(
+            p.node.specifiers,
+            j.stringLiteral(p.value.source.value.replace('/dist', '')),
+        );
+    });
+
+    return source.toSource({ quote: 'single' });
+};
+
+export default transformPaths;
