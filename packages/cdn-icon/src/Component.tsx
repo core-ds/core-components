ea8fb891024e4050c65ee49b428685bdf7271763
EGNKupava <kupavaegn@gmail.com>
2022-07-21 16:59:12 +0300

    feat: new cdn alfabank.servicecdn.ru
    Изменил alfabank.gcdn.co на alfabank.servicecdn.ru
    
    BREAKING CHANGE: Добавьте новый домен в список разрешенных 'img-src': `'self' alfabank.servicecdn.ru
    data: 'self'`

--- packages/cdn-icon/src/Component.tsx
+++ packages/cdn-icon/src/Component.tsx
@@ -27,7 +27,7 @@ export const CDNIcon: React.FC<CDNIconProps> = ({ name, color, dataTestId, class
 
     useEffect(() => {
         const xhr = new XMLHttpRequest();
-        xhr.open('GET', `https://alfabank.gcdn.co/icons/${name}.svg`);
+        xhr.open('GET', `https://alfabank.servicecdn.ru/icons/${name}.svg`);
         xhr.send();
         xhr.onload = function onload() {
             const svg = xhr.response;
