646ca3d8ba3b0b0c9226d84c212611f3cc7663f6
Vladimir Gevak <VGevak@alfabank.ru>
2022-08-11 19:31:53 +0700

    chore: fixed textarea screenshot test

--- packages/textarea/src/component.screenshots.test.tsx
+++ packages/textarea/src/component.screenshots.test.tsx
@@ -7,72 +7,70 @@ const screenshotTesting = setupScreenshotTesting({
     expect,
 });
 
-describe('Textarea', () => {
-    test.skip('Sprite', () => {
-        const testCase = (theme: string) =>
-            screenshotTesting({
-                cases: [
-                    [
-                        `${theme} - main props`,
-                        createSpriteStorybookUrl({
-                            componentName: 'Textarea',
-                            knobs: {
-                                value: 'Компонент текстового поля ввода.',
-                                block: true,
-                                size: ['s', 'm', 'l', 'xl'],
-                                label: ['', 'Лейбл'],
-                            },
-                            size: { width: 240, height: 100 },
-                        }),
-                    ],
-                    [
-                        'inverted',
-                        createSpriteStorybookUrl({
-                            componentName: 'Textarea',
-                            knobs: {
-                                value: 'Компонент текстового поля ввода.',
-                                block: true,
-                                label: 'Лейбл',
-                                colors: 'inverted',
-                                error: ['', 'error'],
-                            },
-                            size: { width: 240, height: 100 },
-                        }),
-                    ],
-                    [
-                        `${theme} - hint & error`,
-                        createSpriteStorybookUrl({
-                            componentName: 'Textarea',
-                            knobs: {
-                                value: 'Компонент текстового поля ввода.',
-                                block: true,
-                                error: ['', 'Ошибка'],
-                                hint: ['', 'Подсказка'],
-                            },
-                            size: { width: 240, height: 100 },
-                        }),
-                    ],
-                    [
-                        `${theme} - counter`,
-                        createSpriteStorybookUrl({
-                            componentName: 'Textarea',
-                            knobs: {
-                                value: 'Компонент текстового поля ввода.',
-                                block: true,
-                                showCounter: true,
-                                maxLength: 500,
-                            },
-                            size: { width: 240, height: 100 },
-                        }),
-                    ],
+describe('Textarea | sprite', () => {
+    const testCase = (theme: string) =>
+        screenshotTesting({
+            cases: [
+                [
+                    `${theme} - main props`,
+                    createSpriteStorybookUrl({
+                        componentName: 'Textarea',
+                        knobs: {
+                            value: 'Компонент текстового поля ввода.',
+                            block: true,
+                            size: ['s', 'm', 'l', 'xl'],
+                            label: ['', 'Лейбл'],
+                        },
+                        size: { width: 240, height: 100 },
+                    }),
                 ],
-                screenshotOpts: {
-                    fullPage: true,
-                },
-                viewport: { width: 1024, height: 100 },
-                theme,
-            })();
+                [
+                    'inverted',
+                    createSpriteStorybookUrl({
+                        componentName: 'Textarea',
+                        knobs: {
+                            value: 'Компонент текстового поля ввода.',
+                            block: true,
+                            label: 'Лейбл',
+                            colors: 'inverted',
+                            error: ['', 'error'],
+                        },
+                        size: { width: 240, height: 100 },
+                    }),
+                ],
+                [
+                    `${theme} - hint & error`,
+                    createSpriteStorybookUrl({
+                        componentName: 'Textarea',
+                        knobs: {
+                            value: 'Компонент текстового поля ввода.',
+                            block: true,
+                            error: ['', 'Ошибка'],
+                            hint: ['', 'Подсказка'],
+                        },
+                        size: { width: 240, height: 100 },
+                    }),
+                ],
+                [
+                    `${theme} - counter`,
+                    createSpriteStorybookUrl({
+                        componentName: 'Textarea',
+                        knobs: {
+                            value: 'Компонент текстового поля ввода.',
+                            block: true,
+                            showCounter: true,
+                            maxLength: 500,
+                        },
+                        size: { width: 240, height: 100 },
+                    }),
+                ],
+            ],
+            screenshotOpts: {
+                fullPage: true,
+            },
+            viewport: { width: 1024, height: 100 },
+            theme,
+        })();
 
-        ['default', 'click'].map(testCase);
-    });
+    ['default', 'click'].map(testCase);
 });
