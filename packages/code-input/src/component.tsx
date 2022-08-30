e01831426462b59c556fc29045f4cc0623525128
Валерия Чуричева <valeriacuriceva@MacBook-Pro-Valeria.local>
2022-08-30 14:48:02 +0300

    feat(code-input): update mobile version

--- packages/code-input/src/component.tsx
+++ packages/code-input/src/component.tsx
@@ -196,6 +196,7 @@ export const CodeInput = forwardRef<CustomInputRef, CodeInputProps>(
 
             const prevRef = inputRefs[prevIndex];
             const nextRef = inputRefs[nextIndex];
+            const curtRef = inputRefs[index];
 
             const newValues = [...values];
 
@@ -209,6 +210,26 @@ export const CodeInput = forwardRef<CustomInputRef, CodeInputProps>(
                         newValues[prevIndex] = '';
 
                         focusOnInput(prevRef);
+                    }
+
+                    setValues(newValues);
+
+                    triggerChange(newValues);
+
+                    break;
+                case 'Delete':
+                    event.preventDefault();
+
+                    if (values[nextIndex]) {
+                        newValues[index] = '';
+                        focusOnInput(nextRef);
+                    } else {
+                        newValues[index] = '';
+                        focusOnInput(curtRef);
+
+                        if (nextRef) {
+                            focusOnInput(nextRef);
+                        }
                     }
 
                     setValues(newValues);
@@ -294,7 +315,7 @@ export const CodeInput = forwardRef<CustomInputRef, CodeInputProps>(
                             onFocus={handleFocus}
                             onKeyDown={handleKeyDown}
                             className={styles.input}
-                            compact={fields > 6}
+                            compact={fields > 8}
                         />
                     ))}
                 </div>
