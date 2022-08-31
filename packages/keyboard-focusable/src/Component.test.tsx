63c30b11c7b34983cc4a717f316f60bb99d7a15c
Alex Yatsenko <reme3d2y@gmail.com>
2022-08-31 13:22:29 +0700

    test: remove unused

--- packages/keyboard-focusable/src/Component.test.tsx
+++ packages/keyboard-focusable/src/Component.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { act, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { KeyboardFocusable } from './index';
