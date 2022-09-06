6f24cbb433c4ced85986d5f0e0b3bc1289e0fb8d
Daniil <48432687+CrybabyDanChan@users.noreply.github.com>
2022-09-06 18:46:26 +0300

    fix(link): replace component with a button in pseudo mode (#156)
    * fix(link): replace component with a button in pseudo mode
    
    BREAKING CHANGE: В компоненте Link с пропсом pseudo заменяется дефолтный html-элемент "a" на
    "button"
    
    Co-authored-by: crybabydanchan <crysiscaramel@gmal.com>

--- packages/link/src/Component.tsx
+++ packages/link/src/Component.tsx
@@ -90,7 +90,7 @@ export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
             children,
             colors = 'default',
             href,
-            Component = 'a',
+            Component = pseudo ? 'button' : 'a',
             ...restProps
         },
         ref,
