d7f4abdfd34b63d977e84dfa7373ae2d1554e3e0
Vladimir <ikuz58@gmail.com>
2022-09-12 22:43:12 +0700

    fix(select): removed ssr warning (#248)

--- packages/select/src/components/base-select/Component.tsx
+++ packages/select/src/components/base-select/Component.tsx
@@ -7,8 +7,9 @@ import React, {
     KeyboardEvent,
     FocusEvent,
     useEffect,
-    useLayoutEffect,
 } from 'react';
+// eslint-disable-next-line @typescript-eslint/camelcase
+import { useLayoutEffect_SAFE_FOR_SSR } from '@alfalab/hooks';
 import { ResizeObserver as ResizeObserverPolyfill } from '@juggle/resize-observer';
 import mergeRefs from 'react-merge-refs';
 import cn from 'classnames';
@@ -342,7 +343,12 @@ export const BaseSelect = forwardRef(
             };
         }, [calcOptionsListWidth, open, optionsListWidth]);
 
-        useLayoutEffect(calcOptionsListWidth, [open, optionsListWidth, options, selectedItems]);
+        useLayoutEffect_SAFE_FOR_SSR(calcOptionsListWidth, [
+            open,
+            optionsListWidth,
+            options,
+            selectedItems,
+        ]);
 
         const renderValue = useCallback(
             () =>
