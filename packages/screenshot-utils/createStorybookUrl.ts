f4be383471400e75f495b631cc650a2ec1f28864
Vladimir <ikuz58@gmail.com>
2022-11-01 10:15:10 +0700

    chore: fix picker-button screenshot (#316)

--- packages/screenshot-utils/createStorybookUrl.ts
+++ packages/screenshot-utils/createStorybookUrl.ts
@@ -58,7 +58,7 @@ export function createStorybookUrl({
     }
 
     const componentPath = subComponentName
-        ? `-${packageName}--${kebab(subComponentName)}`
+        ? `-${packageName.replace(/-/g, '')}--${kebab(subComponentName)}`
         : `-${packageName.replace(/-/g, '')}--${kebab(componentName)}`;
 
     return `${url}?id=компоненты${componentPath}${knobsQuery}&mockDate=${mockDate || ''}`;
