23c0c651eecb730d27857daf44f5618ab6366ba8
Vladimir Gevak <ikuz58@gmail.com>
2022-11-11 12:37:01 +0700

    chore: screenshots in docker

--- bin/run-screenshots.sh
+++ bin/run-screenshots.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
+check_error () {
+  if [ ! "$?" -eq "0" ]
+  then
+    docker stop playwright;
+    exit 1;
+  fi
+}
+
 if [ ! -d "./build" ]
 then
     yarn build-storybook-from-dist
+    check_error
 fi
 
 docker run --name playwright -d -it --rm --ipc=host -v $(pwd):/core-components -w /core-components  mcr.microsoft.com/playwright:v1.27.1-jammy
 
+check_error
+
 docker exec playwright bash -c "apt-get update; apt-get install fonts-roboto -y; apt-get install -y fonts-dejavu-core; fc-cache -fv"
+
+check_error
 
 docker exec -d playwright yarn serve-storybook
 
+check_error
+
 docker exec playwright yarn jest --config=jest.screenshots.config.js "$@"
 
-if [ "$?" -eq "0" ]
-then
-  docker stop playwright;
-  exit 0;
-else
-  docker stop playwright;
-  exit 1;
-fi
+check_error
+
+docker stop playwright
 
