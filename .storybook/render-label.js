7ebdeba4f19cb6428da98e763db9f61f8b8f054a
EGNKupava <kupavaegn@gmail.com>
2022-11-11 12:50:14 +0300

    docs: some improvements

--- .storybook/render-label.js
+++ .storybook/render-label.js
@@ -7,17 +7,27 @@ import { DocumentBlankIcon } from './public/icons/DocumentBlankIcon';
 import { DiamondsBlankIcon } from './public/icons/DiamondsBlankIcon';
 import PencilIcon from './public/images/pencil.svg';
 
+// Через css не удалось выделить нужную группу
+const Gap = () => <span style={{ width: '16px' }} />;
+
 const renderIcon = (type, parent, depth, name) => {
     switch (type) {
         case 'component': {
             if (name === 'Песочница') {
-                // Почему-то не хочет отображать svg компонентом
+                // Иконка карандаша не отображается svg компонентом
                 return <img src={PencilIcon} className='sidebar-icon' />;
             }
             if (parent === 'инструкции-доступность') {
                 return <DocumentBlankIcon />;
             }
-            return parent && parent.includes('компоненты') ? <ComponentIcon /> : <DocumentIcon />;
+            return parent && parent.includes('компоненты') ? (
+                <>
+                    <Gap />
+                    <ComponentIcon />
+                </>
+            ) : (
+                <DocumentIcon />
+            );
         }
         case 'story': {
             return depth > 2 ? <DiamondsBlankIcon /> : <ComponentIcon />;
