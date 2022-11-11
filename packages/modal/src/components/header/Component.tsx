f5984681cd5dbd42a9b0c94e409fe2d47c0efb93
Vladimir <ikuz58@gmail.com>
2022-11-11 17:43:28 +0700

    fix(modal): fixed responsive component (#352)

--- packages/modal/src/components/header/Component.tsx
+++ packages/modal/src/components/header/Component.tsx
@@ -1,10 +1,16 @@
 import React, { FC, ReactNode, useContext, useEffect } from 'react';
 import cn from 'classnames';
 
+import { CrossMIcon } from '@alfalab/icons-glyph/CrossMIcon';
+
 import { getDataTestId } from '../../../../utils';
 import { ModalContext } from '../../Context';
+import { ResponsiveContext } from '../../ResponsiveContext';
+import { Closer } from '../closer/Component';
 
+import desktopStyles from './desktop.module.css';
 import styles from './index.module.css';
+import mobileStyles from './mobile.module.css';
 
 export type HeaderProps = {
     /**
@@ -18,9 +24,9 @@ export type HeaderProps = {
     leftAddons?: ReactNode;
 
     /**
-     * Компонент крестика
+     * Наличие компонента крестика
      */
-    closer?: ReactNode;
+    hasCloser?: ReactNode;
 
     /**
      * Дополнительный класс
@@ -72,11 +78,12 @@ export const Header: FC<HeaderProps> = ({
     align = 'left',
     trim = true,
     title,
-    closer,
+    hasCloser = true,
     sticky,
     dataTestId,
 }) => {
     const { headerHighlighted, setHasHeader } = useContext(ModalContext);
+    const { size, view } = useContext(ResponsiveContext);
 
     const hasContent = title || Boolean(children);
 
@@ -90,15 +97,24 @@ export const Header: FC<HeaderProps> = ({
                 [styles.highlighted]: hasContent && sticky && headerHighlighted,
                 [styles.sticky]: sticky,
                 [styles.hasContent]: hasContent,
+                [desktopStyles.header]: view === 'desktop',
+                [desktopStyles.hasContent]: view === 'desktop' && hasContent,
+                [desktopStyles.sticky]: view === 'desktop' && sticky,
+                [desktopStyles[size]]: view === 'desktop',
+                [mobileStyles.sticky]: view === 'mobile' && sticky,
             })}
             data-test-id={getDataTestId(dataTestId)}
         >
-            {leftAddons && <div className={cn(styles.addon, addonClassName)}>{leftAddons}</div>}
+            {(leftAddons || view === 'desktop') && (
+                <div className={cn(styles.addon, addonClassName)}>{leftAddons}</div>
+            )}
 
             {hasContent && (
                 <div
                     className={cn(styles.content, contentClassName, styles[align], {
                         [styles.trim]: trim,
+                        [desktopStyles.content]: view === 'desktop',
+                        [mobileStyles.content]: view === 'mobile',
                     })}
                 >
                     {children}
@@ -113,8 +129,18 @@ export const Header: FC<HeaderProps> = ({
                 </div>
             )}
 
-            {closer && (
-                <div className={cn(styles.addon, styles.closer, addonClassName)}>{closer}</div>
+            {hasCloser && (
+                <div className={cn(styles.addon, styles.closer, addonClassName)}>
+                    {view === 'desktop' ? (
+                        <Closer dataTestId={getDataTestId(dataTestId, 'closer')} />
+                    ) : (
+                        <Closer
+                            icon={CrossMIcon}
+                            size='xs'
+                            dataTestId={getDataTestId(dataTestId, 'closer')}
+                        />
+                    )}
+                </div>
             )}
         </div>
     );
