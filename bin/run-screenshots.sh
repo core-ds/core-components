79afc6fff02335591437851a286c8b6b2225442c
Vladimir <ikuz58@gmail.com>
2022-11-30 18:42:53 +0700

    chore: updated run-screenshots script (#391)

--- bin/run-screenshots.sh
+++ bin/run-screenshots.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+CONTAINER_NAME='playwright'
+
 check_error () {
   if [ ! "$?" -eq "0" ]
   then
-    docker stop playwright;
+    docker stop $CONTAINER_NAME;
     exit 1;
   fi
 }
@@ -14,20 +16,27 @@ then
     check_error
 fi
 
-docker run --name playwright -d -it --rm --ipc=host -v $(pwd):/core-components -w /core-components  mcr.microsoft.com/playwright:v1.27.1-jammy
+
+if [ ! "$(docker ps -a -q -f name=^/${CONTAINER_NAME}$)" ]
+  then
+    # Если контейнера нет, то создаем новый
+    docker run --name $CONTAINER_NAME -d -it --ipc=host -v "$(pwd)":/core-components -w /core-components  mcr.microsoft.com/playwright:v1.28.1-jammy
+    check_error
+    docker exec $CONTAINER_NAME bash -c "apt-get update; apt-get install fonts-roboto -y; apt-get install -y fonts-dejavu-core; fc-cache -f"
+
+  else
+    # Если контейнер есть, то запускаем существующий
+    docker start $CONTAINER_NAME
+  fi
 
 check_error
 
-docker exec playwright bash -c "apt-get update; apt-get install fonts-roboto -y; apt-get install -y fonts-dejavu-core; fc-cache -f"
+docker exec -d $CONTAINER_NAME yarn serve-storybook
 
 check_error
 
-docker exec -d playwright yarn serve-storybook
+docker exec $CONTAINER_NAME yarn jest --config=jest.screenshots.config.js "$@"
 
 check_error
 
-docker exec playwright yarn jest --config=jest.screenshots.config.js "$@"
-
-check_error
-
-docker stop playwright
+docker stop $CONTAINER_NAME
