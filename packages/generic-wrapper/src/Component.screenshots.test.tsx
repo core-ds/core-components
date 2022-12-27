dfd9804a187adc4d7c39dfa77bc806f13a5101d9
David Sevikian <david.sevikian.work@gmail.com>
2022-12-27 21:59:03 +0800

    test(generic-wrapper): add screenshot tests

--- packages/generic-wrapper/src/Component.screenshots.test.tsx
+++ packages/generic-wrapper/src/Component.screenshots.test.tsx
@@ -1,4 +1,9 @@
-import { setupScreenshotTesting, createStorybookUrl } from '../../screenshot-utils';
+import {
+    setupScreenshotTesting,
+    createStorybookUrl,
+    createSpriteStorybookUrl,
+} from '../../screenshot-utils';
+import React from 'react';
 
 const screenshotTesting = setupScreenshotTesting({
     it,
@@ -18,6 +23,39 @@ describe(
                     testStory: false,
                 }),
             ],
+            [
+                'with column',
+                createStorybookUrl({
+                    componentName: 'GenericWrapper',
+                    testStory: false,
+                    knobs: { column: true },
+                }),
+            ],
+            [
+                'with grow',
+                createStorybookUrl({
+                    componentName: 'GenericWrapper',
+                    testStory: false,
+                    knobs: {
+                        'grow (1st element)': true,
+                        'grow (2st element)': true,
+                        'grow (3st element)': true,
+                    },
+                }),
+            ],
+            [
+                'with padding',
+                createStorybookUrl({
+                    componentName: 'GenericWrapper',
+                    testStory: false,
+                    knobs: {
+                        'padding.top': 'm',
+                        'padding.right': 'm',
+                        'padding.bottom': 'm',
+                        'padding.left': 'm',
+                    },
+                }),
+            ],
         ],
         screenshotOpts: {
             clip: {
@@ -26,6 +64,30 @@ describe(
                 width: 1000,
                 height: 500,
             },
+        },
+    }),
+);
+
+describe(
+    'GenericWrapper | sprites',
+    screenshotTesting({
+        cases: [
+            [
+                'justifyContent',
+                createSpriteStorybookUrl({
+                    componentName: 'GenericWrapper',
+                    packageName: 'generic-wrapper',
+                    knobs: {
+                        justifyContent: ['between', 'around', 'evenly', 'center', 'start', 'end'],
+                        children: 'Мягкая французская булочка',
+                    },
+                    size: { width: 300, height: 30 },
+                }),
+            ],
+        ],
+        viewport: {
+            width: 1000,
+            height: 180,
         },
     }),
 );
