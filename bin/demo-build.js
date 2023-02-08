f2c04c7a42a1efc8726a5e810a40882083ddcb88
Vladimir <ikuz58@gmail.com>
2023-02-08 12:35:14 +0700

    chore: demo-build debug data

--- bin/demo-build.js
+++ bin/demo-build.js
@@ -27,13 +27,21 @@ const execOptions = {
 
 const lastCommitHash = shell.exec('git rev-parse HEAD', execOptions).stdout.trim();
 /** Current git branch */
-const sourceBranch = process.env.GITHUB_HEAD_REF || process.env.GITHUB_REF_NAME;
+const sourceBranch =
+    process.env.GITHUB_HEAD_REF ||
+    process.env.GITHUB_REF_NAME ||
+    shell.exec('git rev-parse --abbrev-ref HEAD', execOptions).stdout.trim();
+console.log(`Source branch = ${sourceBranch}`);
+
 /** Temporary dir for builded file = branch name + last git commit hash */
 const tempOutputDir = sourceBranch.replace(/[^a-zA-Z0-9]/g, '_') + '_' + lastCommitHash;
 /** Try to get affected component name from last commit message **/
 const lastCommitMessage = shell.exec('git show-branch --no-name HEAD', execOptions).stdout.trim();
+console.log(`Last commit message = ${lastCommitMessage}`);
+
 /** Parse affected package from last commit message */
 const affectedPackage = parseScopeFromCommit(lastCommitMessage);
+console.log(`Affected package = ${affectedPackage}`);
 
 /** Git remote url */
 const gitUrl = shell
