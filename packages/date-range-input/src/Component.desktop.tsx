26ccb6fd774a98ff32b6c23f7b87b2ec503d3046
Vladimir <ikuz58@gmail.com>
2023-02-09 15:04:36 +0700

    fix: fixed error ref does not exist on type

--- packages/date-range-input/src/Component.desktop.tsx
+++ packages/date-range-input/src/Component.desktop.tsx
@@ -1,11 +1,12 @@
-import React, { FC } from 'react';
+import React, { forwardRef } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { InputProps } from '@alfalab/core-components-input';
+
 import { DateRangeInput, DateRangeInputProps } from './components/date-range-input/Component';
 
 export type DateRangeInputDesktopProps = Omit<DateRangeInputProps, 'view'>;
 
-export const DateRangeInputDesktop: FC<DateRangeInputDesktopProps> = (props) => (
-    <DateRangeInput {...props} />
+export const DateRangeInputDesktop = forwardRef<HTMLInputElement, DateRangeInputDesktopProps>(
+    (props, ref) => <DateRangeInput {...props} ref={ref} />,
 );
