ab8882877e4bb6251141caefa2e59cff82f97349
Valeri8888 <87258678+Valeri8888@users.noreply.github.com>
2023-02-16 09:11:45 +0300

    feat(underlay): added new component Underlay (#511)
    Co-authored-by: Валерия Чуричева <valeriacuriceva@MacBook-Pro-Valeria.local>

---
+++ packages/underlay/src/Component.tsx
@@ -0,0 +1,56 @@
+import React, { forwardRef } from 'react';
+import cn from 'classnames';
+
+import { UnderlayProps } from './types';
+
+import styles from './index.module.css';
+
+export const Underlay = forwardRef<HTMLDivElement, UnderlayProps>(
+    (
+        {
+            children,
+            borderRadius,
+            shadow,
+            borderSize,
+            backgroundColor,
+            borderColor,
+            className,
+            padding,
+            dataTestId,
+            ...restProps
+        },
+        ref,
+    ) => {
+        const paddingSize =
+            typeof padding === 'string'
+                ? { top: padding, left: padding, right: padding, bottom: padding }
+                : padding;
+
+        const paddingStyles = paddingSize && {
+            [styles[`padding-top-${paddingSize.top}`]]: paddingSize.top,
+            [styles[`padding-right-${paddingSize.right}`]]: paddingSize.right,
+            [styles[`padding-bottom-${paddingSize.bottom}`]]: paddingSize.bottom,
+            [styles[`padding-left-${paddingSize.left}`]]: paddingSize.left,
+        };
+
+        return (
+            <div
+                ref={ref}
+                className={cn(
+                    styles.component,
+                    paddingStyles,
+                    backgroundColor && styles[`background-${backgroundColor}`],
+                    borderRadius && styles[`border-radius-${borderRadius}`],
+                    borderColor && styles[`border-color-${borderColor}`],
+                    borderSize && styles[`border-width-${borderSize}`],
+                    shadow && styles[shadow],
+                    className,
+                )}
+                data-test-id={dataTestId}
+                {...restProps}
+            >
+                {children}
+            </div>
+        );
+    },
+);
