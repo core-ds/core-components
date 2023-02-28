1dd5745d8c4a52dda92b011bf6b55278d913d559
Alex <a.zabalotnikau@gmail.com>
2023-02-28 12:26:56 +0300

    feat(segmented-control): add component (#528)
    * feat(segmented-control): add component
    
    * fix(segmented-control): review fixes
    
    * fix(segmented-control): review fixes

---
+++ .changeset/yellow-stingrays-greet.md
@@ -0,0 +1,5 @@
+---
+'@alfalab/core-components-segmented-control': major
+---
+
+Добавлен новый компонент SegmentedControl.
---
+++ packages/segmented-control/src/Component.test.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { Segment, SegmentedControl, SegmentedControlProps } from './index';
+
+const SEGMENT_TEST_ID = 'SEGMENT';
+
+const renderComponent = ({
+    selectedId,
+    onChange,
+    ...restProps
+}: Omit<SegmentedControlProps, 'children'>) => (
+    <SegmentedControl onChange={onChange} selectedId={selectedId} {...restProps}>
+        <Segment id={1} title={'Label 1'} />
+        <Segment dataTestId={SEGMENT_TEST_ID} id={2} title={'Label 2'} />
+        <Segment id={3} title={'Label 3'} />
+        <Segment id={4} title={'Label 4'} />
+    </SegmentedControl>
+);
+
+describe('segmented-control', () => {
+    it('should display correctly', () => {
+        const { container } = render(renderComponent({ onChange: () => null, selectedId: 1 }));
+        expect(container).toMatchSnapshot();
+    });
+
+    it('should onChange get correct id', () => {
+        const onChange = jest.fn();
+        render(
+            renderComponent({
+                onChange,
+                selectedId: 1,
+            }),
+        );
+
+        fireEvent.click(screen.getByTestId(SEGMENT_TEST_ID));
+
+        expect(onChange).toBeCalledWith(2);
+    });
+
+    it('should set dataTestId', () => {
+        const dataTestId = 'data-test-id';
+        const { getByTestId } = render(
+            renderComponent({ selectedId: 2, onChange: () => null, dataTestId }),
+        );
+
+        expect(getByTestId(dataTestId)).toBeInTheDocument();
+    });
+
+    it('should set className', () => {
+        const className = 'customClassName';
+        const { container } = render(
+            renderComponent({
+                onChange: () => null,
+                className,
+                selectedId: 2,
+            }),
+        );
+
+        expect(container.firstElementChild).toHaveClass(className);
+    });
+});
