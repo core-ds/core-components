b5c9c8efa98bdbba6a891405ee11aa45a0c4281c
Vladimir <ikuz58@gmail.com>
2023-03-07 16:29:42 +0700

    fix(number-input): allowed to call onChange with bad initial state (#565)

--- packages/number-input/src/utils.ts
+++ packages/number-input/src/utils.ts
@@ -4,3 +4,43 @@ export const SEPARATORS = [',', '.'];
 export function createSeparatorsRegExp() {
     return new RegExp(`[${SEPARATORS.map((s) => `\\${s}`).join('')}]`, 'g');
 }
+
+const getNumberRegExp = (fractionLength?: number): RegExp => {
+    let reStr = '[0-9]+';
+
+    if (fractionLength !== 0) {
+        reStr = `${reStr}[${SEPARATORS.map((s) => `\\${s}`).join('')}]?[0-9]{0,${
+            fractionLength || Number.MAX_SAFE_INTEGER
+        }}`;
+    }
+
+    return new RegExp(`^${reStr}$`);
+};
+
+/**
+ * Проверка вводимых значений.
+ */
+export const getAllowedValue = ({
+    value = '',
+    fractionLength,
+    separator,
+    allowSigns,
+}: {
+    value: string;
+    fractionLength?: number;
+    separator: string;
+    allowSigns: boolean;
+}): string => {
+    const sign = allowSigns && SIGNS.some((s) => s === value[0]) ? value[0] : '';
+    const testedValue = sign ? value.slice(1) : value;
+
+    if (getNumberRegExp(fractionLength).test(testedValue)) {
+        return value;
+    }
+
+    const [majorPart, minorPart] = testedValue
+        .split(separator)
+        .map((v) => v.replace(/[^0-9]/g, ''));
+
+    return `${sign}${majorPart}${minorPart ? separator + minorPart.slice(0, fractionLength) : ''}`;
+};
