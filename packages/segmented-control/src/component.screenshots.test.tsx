f9d4bb2702dd7c33a100e57a79e206469c92e3d7
Валерия Чуричева <valeriacuriceva@MacBook-Pro-Valeria.local>
2023-04-17 11:40:18 +0300

    fix(segmented-control): fixed rounding radius for rectangular shape

---
+++ packages/segmented-control/src/component.screenshots.test.tsx
@@ -0,0 +1,26 @@
+import { setupScreenshotTesting, generateTestCases, createPreview } from '../../screenshot-utils';
+
+const screenshotTesting = setupScreenshotTesting({
+    it,
+    beforeAll,
+    afterAll,
+    expect,
+});
+
+describe(
+    'SegmentedControl | main props',
+    screenshotTesting({
+        cases: generateTestCases({
+            componentName: 'SegmentedControl',
+            testStory: false,
+            knobs: {
+                size: ['xs', 'xxs'],
+                shape: ['rounded', 'rectangular'],
+                selectedId: 1,
+            },
+        }),
+        screenshotOpts: {
+            clip: { x: 0, y: 0, width: 1200, height: 200 },
+        },
+    }),
+);
