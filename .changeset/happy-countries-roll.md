344df6bfdc57eb9eecc5f16e429b6c415153425e
Vladimir <ikuz58@gmail.com>
2023-04-25 12:32:23 +0700

    feat(pattern-lock): added new prop extraBounds

---
+++ .changeset/happy-countries-roll.md
@@ -0,0 +1,5 @@
+---
+'@alfalab/core-components-pattern-lock': minor
+---
+
+Добавлен новый проп extraBounds, за счет которого можно увеличить площадь прослушивания события touchMove
--- packages/pattern-lock/src/consts.ts
+++ packages/pattern-lock/src/consts.ts
@@ -1,4 +1,6 @@
-import { ObservableTokens } from './typings';
+import type { ReactPatternLockProps } from 'react-canvas-pattern-lock';
+
+import type { ObservableTokens } from './typings';
 
 export const OBSERVE_OPTIONS: MutationObserverInit = {
     childList: true,
@@ -6,6 +8,8 @@ export const OBSERVE_OPTIONS: MutationObserverInit = {
     characterData: false,
     subtree: true,
 };
+
+export const DEFAULT_EXTRA_BOUNDS: ReactPatternLockProps['extraBounds'] = [75, 50, 75, 50];
 
 export const OBSERVABLE_TOKENS: ObservableTokens = {
     ACCENT_INITIAL: '--color-light-graphic-primary',
