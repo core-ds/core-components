d439d642b4bcec37febc57c4e24282faf4ef4524
Валерия Чуричева <valeriacuriceva@MacBook-Pro-Valeria.local>
2023-06-26 17:58:30 +0300

    feat(indicator): update docs

---
+++ packages/indicator/src/docs/Component.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { select, text, number, boolean } from '@storybook/addon-knobs';
+
+import { Indicator } from '@alfalab/core-components-indicator';
+
+const meta: Meta<typeof Indicator> = {
+    title: 'Components/Indicator',
+    component: Indicator,
+    id: 'Indicator',
+};
+
+type Story = StoryObj<typeof Indicator>;
+
+export const indicator: Story = {
+    name: 'Indicator',
+    render: () => {
+        return (
+            <div
+                style={{
+                    backgroundColor: 'var(--color-light-bg-tertiary)',
+                    padding: '8px',
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                }}
+            >
+                <Indicator
+                    value={number('value', 1)}
+                    height={number('height', undefined)}
+                    size={select('size', ['xs', 's', 'm', 'l'], 's')}
+                    color={text('color', '')}
+                    backgroundColor={text('backgroundColor', '')}
+                    border={boolean('border', true)}
+                    view={select('view', [undefined, 'red', 'grey', 'white'], undefined)}
+                />
+            </div>
+        );
+    },
+};
+
+export default meta;
