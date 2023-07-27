7380c80d5b222fd24eff6756e28febb4169a14a0
borisovartem <i@arborisov.ru>
2023-07-27 15:39:19 +0300

    feat(markdown): added style for wrapper

--- packages/markdown/src/Component.tsx
+++ packages/markdown/src/Component.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import cn from 'classnames';
 
 import { useOverrides } from './hooks/use-overrides';
 import { MarkdownProps } from './typings';
+
+import styles from './index.module.css';
 
 export const Markdown = ({ children, className, platform, font, overrides }: MarkdownProps) => {
     const defaultOverrides = useOverrides(font, platform);
 
     return (
-        <ReactMarkdown components={{ ...defaultOverrides, ...overrides }} className={className}>
+        <ReactMarkdown
+            components={{ ...defaultOverrides, ...overrides }}
+            className={cn(styles.component, className)}
+        >
             {children}
         </ReactMarkdown>
     );
