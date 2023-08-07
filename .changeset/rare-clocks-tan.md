02fc31babcb7be6224c6395924df8ffad2f29394
Valeri8888 <87258678+Valeri8888@users.noreply.github.com>
2023-08-07 12:24:32 +0300

    feat: added mobile and responsive versions components (#734)

---
+++ .changeset/rare-clocks-tan.md
@@ -0,0 +1,5 @@
+---
+"@alfalab/core-components-checkbox-group": major
+---
+
+Для компонента CheckboxGroup добавлены мобильная и адаптивная версии компонента. Responsive компонент теперь экспортируется из индексного файла
--- packages/checkbox/src/Component.screenshots.test.ts
+++ packages/checkbox/src/Component.screenshots.test.ts
@@ -1,4 +1,20 @@
-import { createPreview } from '../../screenshot-utils';
+import { Page } from 'playwright';
+import {
+    setupScreenshotTesting,
+    createSpriteStorybookUrl,
+    generateTestCases,
+    createPreview,
+} from '../../screenshot-utils';
+
+const screenshotTesting = setupScreenshotTesting({
+    it,
+    beforeAll,
+    afterAll,
+    expect,
+});
+
+const clip = { x: 0, y: 0, width: 400, height: 100 };
+const availableThemes = ['default', 'click', 'corp'];
 
 describe('Checkbox', () => {
     createPreview(
@@ -12,4 +28,183 @@ describe('Checkbox', () => {
         },
         'transform:scale(2.3)',
     );
+});
+
+describe('Checkbox | size, disabled, checked', () => {
+    const testCase = (theme: string) =>
+        screenshotTesting({
+            cases: [
+                [
+                    'sprite',
+                    createSpriteStorybookUrl({
+                        componentName: 'Checkbox',
+                        knobs: {
+                            checked: [true, false],
+                            label: ['Согласен с условиями'],
+                            hint: ['Дополнительная информация'],
+                            size: ['s', 'm'],
+                            disabled: [true, false],
+                        },
+                        size: { width: 400, height: 100 },
+                    }),
+                ],
+            ],
+            screenshotOpts: {
+                fullPage: true,
+            },
+            theme,
+        })();
+
+    availableThemes.map(testCase);
+});
+
+describe('Checkbox | inactive, checked', () => {
+    const testCase = (theme: string) =>
+        screenshotTesting({
+            cases: [
+                [
+                    'sprite',
+                    createSpriteStorybookUrl({
+                        componentName: 'Checkbox',
+                        knobs: {
+                            checked: [true, false],
+                            label: ['Согласен с условиями'],
+                            hint: ['Дополнительная информация'],
+                            size: 's',
+                            disabled: [true, false],
+                            inactive: [true, false],
+                        },
+                        size: { width: 400, height: 100 },
+                    }),
+                ],
+            ],
+            screenshotOpts: {
+                fullPage: true,
+            },
+            theme,
+        })();
+
+    availableThemes.map(testCase);
+});
+
+describe('Checkbox | inactive, checked, indeterminate', () => {
+    const testCase = (theme: string) =>
+        screenshotTesting({
+            cases: [
+                [
+                    'sprite',
+                    createSpriteStorybookUrl({
+                        componentName: 'Checkbox',
+                        knobs: {
+                            checked: [true, false],
+                            label: ['Согласен с условиями'],
+                            hint: ['Дополнительная информация'],
+                            size: 's',
+                            disabled: [true, false],
+                            inactive: [true, false],
+                            indeterminate: true,
+                        },
+                        size: { width: 400, height: 100 },
+                    }),
+                ],
+            ],
+            screenshotOpts: {
+                fullPage: true,
+            },
+            theme,
+        })();
+
+    availableThemes.map(testCase);
+});
+
+describe(
+    'Checkbox | addons, block, align',
+    screenshotTesting({
+        cases: [
+            [
+                'sprite',
+                createSpriteStorybookUrl({
+                    componentName: 'Checkbox',
+                    knobs: {
+                        label: ['Согласен с условиями'],
+                        hint: ['Дополнительная информация'],
+                        size: ['s'],
+                        addons: ['', 'Addons'],
+                        block: [true, false],
+                        align: ['start', 'center'],
+                    },
+                    size: { width: 400, height: 100 },
+                }),
+            ],
+        ],
+        screenshotOpts: {
+            fullPage: true,
+        },
+    }),
+);
+
+describe(
+    'Checkbox | hover state',
+    screenshotTesting({
+        cases: generateTestCases({
+            componentName: 'Checkbox',
+            knobs: {
+                label: ['Согласен с условиями'],
+                disabled: [true, false],
+            },
+        }),
+        evaluate: (page: Page) => page.hover('label').then(() => page.waitForTimeout(500)),
+        screenshotOpts: {
+            clip,
+        },
+    }),
+);
+
+describe(
+    'Checkbox | pressed state',
+    screenshotTesting({
+        cases: generateTestCases({
+            componentName: 'Checkbox',
+            knobs: {
+                label: ['Согласен с условиями'],
+                disabled: [true, false],
+            },
+        }),
+        evaluate: (page: Page) => {
+            return page.mouse
+                .move(30, 30)
+                .then(() => page.mouse.down().then(() => page.waitForTimeout(500)));
+        },
+        screenshotOpts: {
+            clip,
+        },
+    }),
+);
+
+describe('Checkbox | checked | error state', () => {
+    const testCase = (theme: string) =>
+        screenshotTesting({
+            cases: [
+                [
+                    'sprite',
+                    createSpriteStorybookUrl({
+                        componentName: 'Checkbox',
+                        knobs: {
+                            checked: [true, false],
+                            label: ['Согласен с условиями'],
+                            hint: ['', 'Дополнительная информация'],
+                            error: ['', 'Ошибка'],
+                            size: ['s'],
+                        },
+                        size: { width: 400, height: 100 },
+                    }),
+                ],
+            ],
+            screenshotOpts: {
+                fullPage: true,
+            },
+            theme,
+        })();
+
+    availableThemes.map(testCase);
 });
