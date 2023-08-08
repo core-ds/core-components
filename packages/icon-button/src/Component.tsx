cbb1d0d7eea7723505e519d3ff18ca30a8116da6
Vladimir <ikuz58@gmail.com>
2023-08-08 17:14:24 +0700

    fix(icon-button): extended icon type (#783)

--- packages/icon-button/src/Component.tsx
+++ packages/icon-button/src/Component.tsx
@@ -1,4 +1,10 @@
-import React, { AnchorHTMLAttributes, ButtonHTMLAttributes, ElementType, forwardRef } from 'react';
+import React, {
+    AnchorHTMLAttributes,
+    ButtonHTMLAttributes,
+    ElementType,
+    forwardRef,
+    ReactElement,
+} from 'react';
 import cn from 'classnames';
 
 import { Button, ButtonProps } from '@alfalab/core-components-button';
@@ -16,7 +22,7 @@ export type IconButtonProps = {
     /**
      * Компонент иконки
      */
-    icon: ElementType<{ className?: string }>;
+    icon: ElementType<{ className?: string }> | ReactElement;
 
     /**
      * Тип кнопки
@@ -81,7 +87,11 @@ export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
             size='s'
         >
             <span className={cn(styles.iconWrapper, styles[size], styles[alignIcon])}>
-                <Icon className={styles.icon} />
+                {React.isValidElement(Icon) ? (
+                    React.cloneElement(Icon, { className: cn(styles.icon, Icon.props.className) })
+                ) : (
+                    <Icon className={styles.icon} />
+                )}
             </span>
         </Button>
     ),
