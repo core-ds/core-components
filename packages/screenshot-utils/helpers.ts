8d91890a2c83073cd46c340ec8fe107b8f465dfa
Vladimir <ikuz58@gmail.com>
2023-08-10 16:05:21 +0700

    chore: update screenshots

--- packages/screenshot-utils/helpers.ts
+++ packages/screenshot-utils/helpers.ts
@@ -117,7 +117,7 @@ export const matchHtml = async ({
     try {
         expect(image).toMatchImageSnapshot({
             comparisonMethod: 'ssim',
-            failureThreshold: 0.0005,
+            failureThreshold: 0.001,
             failureThresholdType: 'percent',
             customSnapshotIdentifier,
             ...matchImageSnapshotOptions,
