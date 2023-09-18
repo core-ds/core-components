bdcf29347ebaac2e6715ac45d426c8532a909851
Valeri8888 <87258678+Valeri8888@users.noreply.github.com>
2023-09-18 18:05:59 +0300

    chore(amount): update docs Amount (#846)
    Co-authored-by: Валерия Чуричева <valeriacuriceva@MacBook-Pro-Valeria.local>

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,4 +4,5 @@ export * from './createPaddingStyle';
 export * from './easingFns';
 export * from './inputUtils';
 export * from './fnUtils';
+export * from './isMobile';
 export * from './maskUtils';
