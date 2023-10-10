ad6470cff1baa9a0593069497948be21412d3535
Vladimir <ikuz58@gmail.com>
2023-10-10 14:19:23 +0700

    fix(input): disableUserInput improvements (#887)

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,6 +2,5 @@ export * from './isClient';
 export * from './getDataTestId';
 export * from './createPaddingStyle';
 export * from './easingFns';
-export * from './inputUtils';
 export * from './fnUtils';
 export * from './maskUtils';
