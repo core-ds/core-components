5495e5be5787c55666704222180bf21395b42d97
Valeri8888 <87258678+Valeri8888@users.noreply.github.com>
2023-10-23 12:22:00 +0300

    chore: update docs HatchingProgressBar,IconButton,KeyboardFocusable,L… (#916)
    Co-authored-by: Валерия Чуричева <valeriacuriceva@MacBook-Pro-Valeria.local>

---
+++ packages/masked-input/src/docs/Component.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { MaskedInput } from '@alfalab/core-components-masked-input';
+
+const mask = [
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+];
+
+const meta: Meta<typeof MaskedInput> = {
+    title: 'Components/MaskedInput',
+    component: MaskedInput,
+    id: 'MaskedInput',
+};
+
+type Story = StoryObj<typeof MaskedInput>;
+
+export const masked_input: Story = {
+    name: 'MaskedInput',
+    render: () => {
+        return (
+            <div style={{ width: '240px' }}>
+                <MaskedInput mask={mask} placeholder='0000 0000 0000 0000' block={true} />
+            </div>
+        );
+    },
+};
+
+export default meta;
