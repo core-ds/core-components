b817e1fdcf6b07d3ebc014023379cb465b5238b9
Валерия Чуричева <valeriacuriceva@MacBook-Pro-Valeria.local>
2023-11-07 13:29:37 +0300

    chore: update docs Alert,ConfirmationV1,IntlPhoneInput,Loader

---
+++ packages/alert/src/docs/Component.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { select, text, boolean } from '@storybook/addon-knobs';
+import { Button } from '@alfalab/core-components-button';
+import { Alert } from '@alfalab/core-components-alert';
+
+const meta: Meta<typeof Alert> = {
+    title: 'Deprecated components/Alert',
+    component: Alert,
+    id: 'Alert',
+};
+
+type Story = StoryObj<typeof Alert>;
+
+export const alert: Story = {
+    name: 'Alert',
+    render: () => (
+        <Alert
+            view={select('view', ['common', 'negative', 'positive', 'attention'], 'common')}
+            title={text('title', '')}
+            hasCloser={boolean('hasCloser', false)}
+            buttons={
+                boolean('buttons', false)
+                    ? [<Button>Хорошо</Button>, <Button>Подробнее</Button>]
+                    : null
+            }
+        >
+            {text('children', 'Вам одобрено. Согласитесь на предложение')}
+        </Alert>
+    ),
+};
+
+export default meta;
