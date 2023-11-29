e24452fb120a8a259c2f87565af9c7530db33f5e
Valeri8888 <87258678+Valeri8888@users.noreply.github.com>
2023-11-29 08:25:49 +0300

    feat(segmented-control): added new colors props (#1000)

--- packages/segmented-control/src/context.ts
+++ packages/segmented-control/src/context.ts
@@ -4,8 +4,10 @@ import { IDType } from './typing';
 
 export type ContextType = {
     onChange: (id: IDType) => void;
+    colors?: 'default' | 'inverted';
 };
 
 export const SegmentedControlContext = createContext<ContextType>({
     onChange: () => null,
+    colors: 'default',
 });
