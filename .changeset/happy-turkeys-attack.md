404e076e2971d94df204f82be946f0970a625af6
Valeri8888 <87258678+Valeri8888@users.noreply.github.com>
2024-02-12 08:07:58 +0300

    feat(text): added new component Техт (#1067)

---
+++ .changeset/happy-turkeys-attack.md
@@ -0,0 +1,5 @@
+---
+"@alfalab/core-components-text": major
+---
+
+Добавлен новый компонент Text
---
+++ packages/text/src/Component.screenshots.test.tsx
@@ -0,0 +1,109 @@
+import { setupScreenshotTesting, createSpriteStorybookUrl } from '../../screenshot-utils';
+
+const screenshotTesting = setupScreenshotTesting({
+    it,
+    beforeAll,
+    afterAll,
+    expect,
+});
+
+describe(
+    'Text | props rowLimit',
+    screenshotTesting({
+        cases: [
+            [
+                'sprite',
+                createSpriteStorybookUrl({
+                    componentName: 'Text',
+                    knobs: {
+                        children:
+                            'Съешь ещё этих мягких французских булок, да выпей чаю. Съешь ещё этих мягких французских булок, да выпей чаю',
+                        rowLimit: [1, 2, 3],
+                    },
+                    size: { width: 250, height: 150 },
+                }),
+            ],
+        ],
+        viewport: { width: 1000, height: 200 },
+    }),
+);
+
+describe(
+    'Text | props view',
+    screenshotTesting({
+        cases: [
+            [
+                'sprite',
+                createSpriteStorybookUrl({
+                    componentName: 'Text',
+                    knobs: {
+                        children: 'Съешь ещё этих мягких французских булок.',
+                        view: [
+                            'paragraph-primary-large',
+                            'paragraph-primary-medium',
+                            'paragraph-primary-small',
+                            'paragraph-secondary-large',
+                            'paragraph-secondary-medium',
+                            'paragraph-secondary-small',
+                            'paragraph-component',
+                            'paragraph-component-primary',
+                            'paragraph-component-secondary',
+                            'paragraph-caps',
+                            'headline-medium',
+                            'headline-system-medium',
+                            'margin-medium',
+                        ],
+                    },
+                    size: { width: 470, height: 100 },
+                }),
+            ],
+        ],
+        viewport: { width: 1500, height: 700 },
+        screenshotOpts: {
+            fullPage: true,
+        },
+    }),
+);
+
+describe(
+    'Text | props color and textBackgroundColor',
+    screenshotTesting({
+        cases: [
+            [
+                'sprite',
+                createSpriteStorybookUrl({
+                    componentName: 'Text',
+                    knobs: {
+                        children: 'Съешь ещё этих мягких французских булок, да выпей чаю.',
+                        color: 'var(--color-light-decorative-text-indigo)',
+                        textBackgroundColor: 'var(--color-light-decorative-muted-alt-indigo)',
+                        tag: 'span',
+                    },
+                    size: { width: 550, height: 100 },
+                }),
+            ],
+        ],
+        viewport: { width: 550, height: 100 },
+    }),
+);
+
+describe(
+    'Text | props align',
+    screenshotTesting({
+        cases: [
+            [
+                'sprite',
+                createSpriteStorybookUrl({
+                    componentName: 'Text',
+                    knobs: {
+                        children: 'Съешь ещё',
+                        align: ['left', 'right', 'center'],
+                        tag: 'div',
+                    },
+                    size: { width: 300, height: 100 },
+                }),
+            ],
+        ],
+        viewport: { width: 1000, height: 100 },
+    }),
+);
