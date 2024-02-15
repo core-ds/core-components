49b7ca4a9456670c81c8afe1873af7b7fb0caec1
Валерия Чуричева <valeriacuriceva@MacBook-Pro-Valeria.local>
2024-02-15 12:09:11 +0300

    fix(pure-cell): fix type subComponents

--- packages/pure-cell/src/components/types.ts
+++ packages/pure-cell/src/components/types.ts
@@ -10,18 +10,20 @@ import { Graphics } from './graphics';
 import { Text } from './text';
 
 type TextElement = ReactElement<typeof Text>;
-export type GraphicsElement = ReactElement<typeof Graphics>;
+export type GraphicsElement = ReactElement<typeof Graphics> | boolean | null;
 type AmountElement = ReactElement<typeof Amount>;
 type AmountTitleElement = ReactElement<typeof AmountTitle>;
 export type CommentElement = ReactElement<typeof Comment>;
 type FooterButtonElement = ReactElement<typeof FooterButton>;
 type FooterTitleElement = ReactElement<typeof FooterText>;
 export type FooterElement =
-    | Array<ReactElement<FooterButtonElement> | FooterTitleElement>
-    | CommentElement;
+    | Array<ReactElement<FooterButtonElement> | FooterTitleElement | boolean | null>
+    | CommentElement
+    | boolean
+    | null;
 export type MainElement =
-    | Array<undefined | '' | TextElement | AmountElement | AmountTitleElement>
-    | (TextElement | AmountElement | AmountTitleElement);
+    | Array<undefined | '' | boolean | null | TextElement | AmountElement | AmountTitleElement>
+    | (TextElement | AmountElement | AmountTitleElement | boolean | null);
 export type ContentElement = Array<MainElement | AddonElement | FooterElement> | MainElement;
 export type AddonElement = Array<TextElement | ReactNode> | TextElement | ReactNode;
 export type PureCellElement = Array<GraphicsElement | ContentElement> | ContentElement;
