8d22273fd0a2b5a8da7779d781d66468993d4ade
u_m1mkt <sowtame@gmail.com>
2024-03-26 11:12:39 +0300

    feat(deploy_kube_feature): deploy to platform

--- packages/provider/src/index.ts
+++ packages/provider/src/index.ts
@@ -1,16 +1,2 @@
-import { createContext } from 'react';
-
-import { PortalProps } from '@alfalab/core-components-portal';
-
 export * from './hooks';
-
-type CoreComponentsContextValue = {
-    /** Уникальный идентификатор страницы для специфичности стилей страницы  */
-    getContainer?: PortalProps['getPortalContainer'];
-};
-
-export const CoreComponentsContext = createContext<CoreComponentsContextValue>({
-    getContainer: undefined,
-});
-
-export const CoreComponentsProvider = CoreComponentsContext.Provider;
+export * from './context';
