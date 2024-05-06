b354e5e8101c53cf6dd41ba0f319f35d577d3019
Mikhail Lukin <mishalykin@mail.ru>
2024-05-06 17:35:17 +0700

    refactor(toast-plate): code clean

--- packages/toast-plate/src/components/base-toast-plate/types/BaseToastPlatePropTypes.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated Нужен для обратной совместимости, в будущих версиях будет удален
- */
-export type BadgeIcons = {
-    negative: JSX.Element;
-    positive: JSX.Element;
-    attention: JSX.Element;
-};
