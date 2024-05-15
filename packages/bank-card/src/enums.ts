8feac9a088230fbe092dfb2e292f755f66d80f3f
fulcanellee <45999900+fulcanellee@users.noreply.github.com>
2024-05-15 22:02:57 +0700

    feat(bank-card): add param for mask type (#1202)
    * feat(bank-card): add param for mask type
    
    * Create spotty-laws-leave.md
    
    * feat(bank-card): add param for mask type
    
    * Update spotty-laws-leave.md
    
    * Delete .changeset/spotty-laws-leave.md
    
    * Create slow-mice-know.md
    
    * feat(bank-card): typo

---
+++ packages/bank-card/src/enums.ts
@@ -0,0 +1,5 @@
+export enum MaskTypeEnum {
+    Card = 'card',
+    AccountNumber = 'account-number',
+    Default = 'default',
+}
---
+++ packages/bank-card/src/helpers/getDefaultInputLabel.tsx
@@ -0,0 +1,14 @@
+import { MaskTypeEnum } from '../enums';
+import { MaskType } from '../types';
+
+export const getDefaultInputLabel = (maskType: MaskType) => {
+    switch (maskType) {
+        case MaskTypeEnum.Card:
+            return 'Номер карты';
+        case MaskTypeEnum.AccountNumber:
+            return 'Номер счёта';
+        case MaskTypeEnum.Default:
+        default:
+            return 'Номер карты или счёта';
+    }
+};
