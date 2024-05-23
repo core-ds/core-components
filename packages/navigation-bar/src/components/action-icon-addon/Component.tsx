fulcanellee/core-components-sandbox

feat(navigation-bar): new component added

packages/navigation-bar/src/components/action-icon-addon/Component.tsx
@@ -1,0 +1,58 @@
+import React from 'react';
+import cn from 'classnames';
+
+import { ButtonDesktop } from '@alfalab/core-components-button/desktop';
+import { ChevronLeftMIcon } from '@alfalab/icons-glyph/ChevronLeftMIcon';
+import { CrossMIcon } from '@alfalab/icons-glyph/CrossMIcon';
+
+import styles from './index.module.css';
+
+export interface ActionIconAddonProps extends React.HTMLAttributes<HTMLButtonElement> {
+    /**
+     * Тип действия
+     */
+    action?: 'back' | 'floatedBack' | 'close';
+
+    /**
+     * Дополнительный класс
+     */
+    className?: string;
+
+    /**
+     * Обработчик клика
+     */
+    onClick?: () => void;
+
+    /**
+     * Идентификатор для систем автоматизированного тестирования.
+     */
+    dataTestId?: string;
+}
+
+const iconComponents = {
+    back: <ChevronLeftMIcon />,
+    floatedBack: <ChevronLeftMIcon className={styles.floating} />,
+    close: <CrossMIcon className={styles.floating} />,
+};
+
+export const ActionIconAddon: React.FC<ActionIconAddonProps> = ({
+    onClick,
+    className,
+    action = 'back',
+    dataTestId,
+    ...htmlAttributes
+}) => (
+    <ButtonDesktop
+        view='text'
+        size='s'
+        onClick={onClick}
+        aria-label={action === 'close' ? 'закрыть' : 'назад'}
+        className={cn(styles.component, className)}
+        dataTestId={dataTestId}
+        {...htmlAttributes}
+    >
+        <div className={cn(styles.iconWrapper, { [styles[action]]: Boolean(styles[action]) })}>
+            {iconComponents[action]}
+        </div>
+    </ButtonDesktop>
+);

core-ds/core-components

packages/navigation-bar/src/components/action-icon-addon/Component.tsx
@@ -1,0 +1,58 @@
+import React from 'react';
+import cn from 'classnames';
+
+import { ButtonDesktop } from '@alfalab/core-components-button/desktop';
+import { ChevronLeftMIcon } from '@alfalab/icons-glyph/ChevronLeftMIcon';
+import { CrossMIcon } from '@alfalab/icons-glyph/CrossMIcon';
+
+import styles from './index.module.css';
+
+export interface ActionIconAddonProps extends React.HTMLAttributes<HTMLButtonElement> {
+    /**
+     * Тип действия
+     */
+    action?: 'back' | 'floatingBack' | 'close';
+
+    /**
+     * Дополнительный класс
+     */
+    className?: string;
+
+    /**
+     * Обработчик клика
+     */
+    onClick?: () => void;
+
+    /**
+     * Идентификатор для систем автоматизированного тестирования.
+     */
+    dataTestId?: string;
+}
+
+const iconComponents = {
+    back: <ChevronLeftMIcon />,
+    floatingBack: <ChevronLeftMIcon className={styles.floating} />,
+    close: <CrossMIcon className={styles.floating} />,
+};
+
+export const ActionIconAddon: React.FC<ActionIconAddonProps> = ({
+    onClick,
+    className,
+    action = 'back',
+    dataTestId,
+    ...htmlAttributes
+}) => (
+    <ButtonDesktop
+        view='text'
+        size='s'
+        onClick={onClick}
+        aria-label={action === 'close' ? 'закрыть' : 'назад'}
+        className={cn(styles.component, className)}
+        dataTestId={dataTestId}
+        {...htmlAttributes}
+    >
+        <div className={cn(styles.iconWrapper, { [styles[action]]: Boolean(styles[action]) })}>
+            {iconComponents[action]}
+        </div>
+    </ButtonDesktop>
+);