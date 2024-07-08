772733bfa6594d3c444b6e44f6c8b16ce924e39c
Eugene Petrov <petrovik@inbox.ru>
2024-07-08 16:43:30 +0300

    feat(navigation-bar): add forwardRef to component
    surround the component by forwardRef

--- packages/navigation-bar/src/Component.tsx
+++ packages/navigation-bar/src/Component.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode, useEffect, useRef, useState } from 'react';
+import React, { forwardRef, ReactNode, useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
 
 import { getDataTestId } from '@alfalab/core-components-shared';
@@ -93,119 +93,126 @@ export interface NavigationBarProps {
     dataTestId?: string;
 }
 
-export const NavigationBar: FC<NavigationBarProps> = ({
-    align = 'center',
-    rightAddons,
-    leftAddons,
-    bottomAddons,
-    sticky,
-    border,
-    subtitle,
-    title,
-    children,
-    backgroundColor = 'var(--color-light-base-bg-primary)',
-    contentWrapperClassName,
-    className,
-    contentClassName,
-    rightAddonsClassName,
-    leftAddonsClassName,
-    bottomAddonsClassName,
-    dataTestId,
-}) => {
-    const [titleMargin, setTitleMargin] = useState({ left: 0, right: 0 });
-    const leftAddonsRef = useRef<HTMLDivElement>(null);
-    const rightAddonsRef = useRef<HTMLDivElement>(null);
-    const hasLeftAddons = leftAddons && align !== 'left';
+export const NavigationBar = forwardRef<HTMLDivElement, NavigationBarProps>(
+    (
+        {
+            align = 'center',
+            rightAddons,
+            leftAddons,
+            bottomAddons,
+            sticky,
+            border,
+            subtitle,
+            title,
+            children,
+            backgroundColor = 'var(--color-light-base-bg-primary)',
+            contentWrapperClassName,
+            className,
+            contentClassName,
+            rightAddonsClassName,
+            leftAddonsClassName,
+            bottomAddonsClassName,
+            dataTestId,
+        },
+        ref,
+    ) => {
+        const [titleMargin, setTitleMargin] = useState({ left: 0, right: 0 });
+        const leftAddonsRef = useRef<HTMLDivElement>(null);
+        const rightAddonsRef = useRef<HTMLDivElement>(null);
+        const hasLeftAddons = leftAddons && align !== 'left';
 
-    useEffect(() => {
-        if (hasLeftAddons) {
-            const leftAddonsWidth = leftAddonsRef.current?.offsetWidth || 0;
-            const rightAddonsWidth = rightAddonsRef.current?.offsetWidth || 0;
+        useEffect(() => {
+            if (hasLeftAddons) {
+                const leftAddonsWidth = leftAddonsRef.current?.offsetWidth || 0;
+                const rightAddonsWidth = rightAddonsRef.current?.offsetWidth || 0;
 
-            const marginSize = Math.abs(rightAddonsWidth - leftAddonsWidth);
-            const shouldAddLeftMargin = rightAddonsWidth - leftAddonsWidth > 0;
+                const marginSize = Math.abs(rightAddonsWidth - leftAddonsWidth);
+                const shouldAddLeftMargin = rightAddonsWidth - leftAddonsWidth > 0;
 
-            setTitleMargin((prev) => {
-                const newState = shouldAddLeftMargin
-                    ? { left: marginSize, right: 0 }
-                    : { left: 0, right: marginSize };
+                setTitleMargin((prev) => {
+                    const newState = shouldAddLeftMargin
+                        ? { left: marginSize, right: 0 }
+                        : { left: 0, right: marginSize };
 
-                const isStateChanged = prev.left !== newState.left || prev.right !== newState.right;
+                    const isStateChanged =
+                        prev.left !== newState.left || prev.right !== newState.right;
 
-                return isStateChanged ? newState : prev;
-            });
-        }
-    }, [hasLeftAddons, leftAddons, rightAddons]);
+                    return isStateChanged ? newState : prev;
+                });
+            }
+        }, [hasLeftAddons, leftAddons, rightAddons]);
 
-    return (
-        <div
-            className={cn(
-                styles.component,
-                {
-                    [styles.border]: border,
-                    [styles.sticky]: sticky,
-                },
-                className,
-            )}
-            style={{
-                ...(backgroundColor && { backgroundColor }),
-            }}
-            data-test-id={dataTestId}
-        >
-            <div className={cn(styles.mainLine, styles[align], contentClassName)}>
-                {hasLeftAddons && (
-                    <div className={cn(styles.addons, leftAddonsClassName)} ref={leftAddonsRef}>
-                        {leftAddons}
-                    </div>
+        return (
+            <div
+                ref={ref}
+                className={cn(
+                    styles.component,
+                    {
+                        [styles.border]: border,
+                        [styles.sticky]: sticky,
+                    },
+                    className,
                 )}
-
-                {children && <div className={styles.children}>{children}</div>}
-
-                {title && (
-                    <div
-                        className={cn(styles.content, contentWrapperClassName, {
-                            [styles[align]]: !hasLeftAddons,
-                        })}
-                        style={{
-                            ...(align === 'center'
-                                ? {
-                                      marginLeft: titleMargin.left,
-                                      marginRight: titleMargin.right,
-                                  }
-                                : null),
-                        }}
-                    >
-                        <div
-                            className={cn(styles.title, {
-                                [styles[align]]: !hasLeftAddons && !subtitle,
-                            })}
-                            data-test-id={getDataTestId(dataTestId, 'title')}
-                        >
-                            {title}
+                style={{
+                    ...(backgroundColor && { backgroundColor }),
+                }}
+                data-test-id={dataTestId}
+            >
+                <div className={cn(styles.mainLine, styles[align], contentClassName)}>
+                    {hasLeftAddons && (
+                        <div className={cn(styles.addons, leftAddonsClassName)} ref={leftAddonsRef}>
+                            {leftAddons}
                         </div>
+                    )}
 
-                        {subtitle && (
+                    {children && <div className={styles.children}>{children}</div>}
+
+                    {title && (
+                        <div
+                            className={cn(styles.content, contentWrapperClassName, {
+                                [styles[align]]: !hasLeftAddons,
+                            })}
+                            style={{
+                                ...(align === 'center'
+                                    ? {
+                                          marginLeft: titleMargin.left,
+                                          marginRight: titleMargin.right,
+                                      }
+                                    : null),
+                            }}
+                        >
                             <div
-                                className={styles.subtitle}
-                                data-test-id={getDataTestId(dataTestId, 'subtitle')}
+                                className={cn(styles.title, {
+                                    [styles[align]]: !hasLeftAddons && !subtitle,
+                                })}
+                                data-test-id={getDataTestId(dataTestId, 'title')}
                             >
-                                {subtitle}
+                                {title}
                             </div>
-                        )}
-                    </div>
-                )}
 
-                {rightAddons && (
-                    <div
-                        className={cn(styles.rightAddons, styles.addons, rightAddonsClassName)}
-                        ref={rightAddonsRef}
-                    >
-                        {rightAddons}
-                    </div>
-                )}
+                            {subtitle && (
+                                <div
+                                    className={styles.subtitle}
+                                    data-test-id={getDataTestId(dataTestId, 'subtitle')}
+                                >
+                                    {subtitle}
+                                </div>
+                            )}
+                        </div>
+                    )}
+
+                    {rightAddons && (
+                        <div
+                            className={cn(styles.rightAddons, styles.addons, rightAddonsClassName)}
+                            ref={rightAddonsRef}
+                        >
+                            {rightAddons}
+                        </div>
+                    )}
+                </div>
+
+                {bottomAddons && <div className={bottomAddonsClassName}>{bottomAddons}</div>}
             </div>
-
-            {bottomAddons && <div className={bottomAddonsClassName}>{bottomAddons}</div>}
-        </div>
-    );
-};
+        );
+    },
+);
