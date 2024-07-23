fa081fc81e771c44875122187c2f16ec77d15c99
Denis 🎃 Khripkov <github.com@denisx.com>
2024-07-23 14:28:40 +0300

    fix: rollback code;

--- bin/analyze-package-sizes.mjs
+++ bin/analyze-package-sizes.mjs
@@ -8,7 +8,6 @@ const packages = shell.exec(
         --ignore @alfalab/core-components-codemod \\
         --ignore @alfalab/core-components-vars \\
         --ignore @alfalab/core-components-themes \\
-        --ignore @loadable/component \\
         --all`,
     { silent: true },
 ).stdout;
--- packages/calendar-input/src/component.screenshots.test.tsx
+++ packages/calendar-input/src/component.screenshots.test.tsx
@@ -20,7 +20,7 @@ describe('CalendarInput', () =>
         'padding: 0 270px;width:800px;transform:scale(2.1)',
         {
             viewport: { width: 1024, height: 600 },
-        },
+        }
     ));
 
 describe('CalendarInput | interactions tests', () => {
