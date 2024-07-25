b5aad25abdb150353c0c75ea70034899608844f0
Mikhail Lukin <mishalykin@mail.ru>
2024-07-25 17:36:11 +0700

    feat(calendar): clickableMonth

--- packages/calendar/src/components/calendar-mobile/Component.tsx
+++ packages/calendar/src/components/calendar-mobile/Component.tsx
@@ -105,7 +105,7 @@ export const CalendarMonthOnlyView = ({
 
     const maxDate = useMemo(() => {
         // блокируем последующие дни после текущего
-        if (clickableMonth) {
+        if (clickableMonth && !maxDateTimestamp) {
             return new Date();
         }
 
