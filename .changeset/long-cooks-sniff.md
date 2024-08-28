a9fa2b82e14d0287c2d10be9d80f1132b0bf2a26
Валерия Чуричева <valeriacuriceva@MacBook-Pro-Valeria.local>
2024-08-28 10:30:37 +0300

    feat(typography): added new prop breakpoint

---
+++ .changeset/long-cooks-sniff.md
@@ -0,0 +1,5 @@
+---
+"@alfalab/core-components-typography": minor
+---
+
+Добавлено новое свойство breakpoint, отвечающее за контрольную точку, с которой начинается desktop версия
--- packages/typography/src/component.tsx
+++ packages/typography/src/component.tsx
@@ -3,13 +3,13 @@ import { FC, ForwardRefExoticComponent, RefAttributes } from 'react';
 import { Text, TextProps } from './text';
 import { Title, TitleProps } from './title';
 import { TitleMobile, TitleMobileProps } from './title-mobile';
-import { TitleResponsive } from './title-responsive';
+import { TitleResponsive, TitleResponsiveProps } from './title-responsive';
 import { TextElementType } from './types';
 
 export const Typography: {
     Title: FC<TitleProps>;
     Text: ForwardRefExoticComponent<TextProps & RefAttributes<TextElementType>>;
-    TitleResponsive: FC<TitleProps>;
+    TitleResponsive: FC<TitleResponsiveProps>;
     TitleMobile: FC<TitleMobileProps>;
 } = {
     Title,
