8b62721bdcabf236d58038d44f08cd503755a76e
fulcanellee <45999900+fulcanellee@users.noreply.github.com>
2024-09-02 17:08:54 +0700

    Feat/checkbox radio size 20 (#1343)
    * feat(checkbox): add size 20, update docs

---
+++ .changeset/six-islands-drive.md
@@ -0,0 +1,9 @@
+---
+'@alfalab/core-components-checkbox': minor
+'@alfalab/core-components-checkbox-group': minor
+'@alfalab/core-components-radio': minor
+'@alfalab/core-components-radio-group': minor
+---
+
+- Стилевые исправления компонентов
+- Обновление документации
---
+++ packages/checkbox/src/icons/icon-check-24.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const IconCheck24 = () => (
+    <svg width='10' height='8' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+        <path
+            fillRule='evenodd'
+            clipRule='evenodd'
+            d='M10 1.5 3.5 8 0 4.5 1.5 3l2 2 5-5L10 1.5Z'
+            fillOpacity='.94'
+        />
+    </svg>
+);
