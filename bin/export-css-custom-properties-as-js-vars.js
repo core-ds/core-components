79b3681b89df9000c527a637419f26a214e1d487
Mikhail Lukin <mishalykin@mail.ru>
2024-09-03 20:14:45 +0700

    feat(vars): add js custom properties

--- bin/export-css-custom-properties-as-js-vars.js
+++ bin/export-css-custom-properties-as-js-vars.js
@@ -10,6 +10,21 @@ const distPath = '../packages/vars/dist';
 fs.mkdirSync(path.resolve(__dirname, distPath), { recursive: true });
 
 const BUILDS = ['es5', 'cssm', 'esm', 'modern'];
+const PALETTES = [
+    'colors',
+    'colors-addons',
+    'colors-bluetint',
+    'colors-decorative',
+    'colors-indigo',
+    'colors-pfm',
+    'colors-qualitative',
+    'colors-sequential',
+    'colors-students',
+    'colors-transparent',
+    'colors-x5',
+    'shadows-bluetint',
+    'shadows-indigo',
+];
 
 function ensureDirectoryExist(filePath) {
     const dirname = path.dirname(filePath);
@@ -46,18 +61,35 @@ function createExporter(buildName) {
     };
 }
 
+/** формирует файлы деклараций d.ts */
+function execTsc(destJsPath) {
+    return function () {
+        console.log('create d.ts for', destJsPath);
+
+        shell.exec(
+            `tsc --typeRoots [] --declaration --emitDeclarationOnly --allowJs ${destJsPath}`,
+        );
+    };
+}
+
+/** формирует путь для создания js модуля с css переменными (учитывая тип сборки) */
+function createJSPathDestination(buildName, srcFileName) {
+    return path.resolve(
+        __dirname,
+        distPath,
+        buildName === 'es5' ? '' : buildName,
+        `${srcFileName}.js`,
+    );
+}
+
+/** собирает css переменные из vars/index.css в единый js модуль */
 function exportCssVarsAsJs(srcFileName) {
     const cssPath = path.resolve(__dirname, `../packages/vars/src/${srcFileName}.css`);
     const css = fs.readFileSync(cssPath, 'utf-8');
 
     return Promise.all(
         BUILDS.map((buildName) => {
-            const destJsPath = path.resolve(
-                __dirname,
-                distPath,
-                buildName === 'es5' ? '' : buildName,
-                `${srcFileName}.js`,
-            );
+            const destJsPath = createJSPathDestination(buildName, srcFileName);
 
             ensureDirectoryExist(destJsPath);
             return postcss()
@@ -69,11 +101,35 @@ function exportCssVarsAsJs(srcFileName) {
                     }),
                 )
                 .process(css, { from: cssPath, to: destJsPath })
-                .then(() => {
-                    shell.exec(
-                        `tsc --typeRoots [] --declaration --emitDeclarationOnly --allowJs ${destJsPath}`,
-                    );
-                });
+                .then(execTsc(destJsPath));
+        }),
+    );
+}
+
+/** собирает js модуль для каждого css файла по отдельности */
+function buildPalettesCustomCSSProperties() {
+    return Promise.all(
+        PALETTES.map((palette) => {
+            const cssPath = path.resolve(__dirname, `../packages/vars/src/${palette}.css`);
+            const css = fs.readFileSync(cssPath, 'utf-8');
+
+            BUILDS.map((buildName) => {
+                const destJsPath = createJSPathDestination(buildName, `${palette}.module`);
+
+                ensureDirectoryExist(destJsPath);
+
+                console.log(`build [ ${palette} - ${buildName} ] module`);
+
+                return postcss()
+                    .use(
+                        exportCustomVariables({
+                            exporter: createExporter(buildName),
+                            destination: destJsPath,
+                        }),
+                    )
+                    .process(css, { from: cssPath, to: destJsPath })
+                    .then(execTsc(destJsPath));
+            });
         }),
     );
 }
@@ -84,3 +140,8 @@ exportCssVarsAsJs('index')
         console.error(reason);
         process.exit(1);
     });
+
+buildPalettesCustomCSSProperties().catch((reason) => {
+    console.error(reason);
+    process.exit(1);
+});
