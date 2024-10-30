6ce76de2929127fa9f58577db914e16d6cf5033d
Mikhail Lukin <mishalykin@mail.ru>
2024-10-30 22:08:58 +0700

    feat: universal-modal add test

--- packages/universal-modal/src/utils/createResponsive.tsx
+++ packages/universal-modal/src/utils/createResponsive.tsx
@@ -3,6 +3,7 @@ import React, { FC, useContext } from 'react';
 import { ResponsiveContext } from '../ResponsiveContext';
 
 export function createResponsive(desktop: FC, mobile: FC) {
+    // eslint-disable-next-line
     function ResponsiveComponent(props: any) {
         const { view = 'desktop' } = useContext(ResponsiveContext) || {};
 
