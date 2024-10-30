9887bcb91017732ea0510ac6d3a0b38a0b90192d
Denis 🎃 Khripkov <github.com@denisx.com>
2024-10-30 14:06:09 +0300

    fix: fix snaps;

--- packages/icon-view/src/component.screenshots.test.tsx
+++ packages/icon-view/src/component.screenshots.test.tsx
@@ -48,7 +48,7 @@ describe(
             componentName: 'IconView',
             subComponentName: 'SuperEllipse',
             knobs: {
-                size: [48, 64, 80, 128, 40, 56],
+                size: [48, 64, 80, 128],
                 topAddons: [true, false],
                 bottomAddons: [true, false],
                 border: [true, false],
@@ -104,7 +104,7 @@ describe(
             componentName: 'IconView',
             subComponentName: 'Circle',
             knobs: {
-                size: [48, 64, 80, 40, 56, 128],
+                size: [48, 64, 80],
                 topAddons: [true, false],
                 bottomAddons: [true, false],
                 border: [true, false],
