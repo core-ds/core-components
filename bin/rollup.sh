73e5ed19a67e7378f331b2b5dcc3779b72ace903
Alex Yatsenko <reme3d2y@gmail.com>
2024-10-31 14:17:55 +0300

    fix: paths

--- bin/rollup.sh
+++ bin/rollup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+t0=$(date +%s)
+
+component=$(pwd | sed 's/.*\///')
+echo "~ Building $component"
+
 ROLLUP_CONFIG_PATH="rollup.config.mjs"
 rollup -c ${LERNA_ROOT_PATH}/${ROLLUP_CONFIG_PATH} --silent
+
+t1=$(date +%s)
+echo "+ Built $component in $((t1 - t0))s"
