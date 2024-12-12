1af34ca696e2eaf0dc872b828286674e4ff10876
Mikhail Lukin <mishalykin@mail.ru>
2024-12-12 16:49:48 +0700

    feat(number-input): fix icons for size 40

--- packages/number-input/src/components/steppers/Component.tsx
+++ packages/number-input/src/components/steppers/Component.tsx
@@ -5,7 +5,9 @@ import { IconButton } from '@alfalab/core-components-icon-button';
 import { InputProps } from '@alfalab/core-components-input';
 import { getDataTestId } from '@alfalab/core-components-shared';
 import { MinusMIcon } from '@alfalab/icons-glyph/MinusMIcon';
+import { MinusSIcon } from '@alfalab/icons-glyph/MinusSIcon';
 import { PlusMediumMIcon } from '@alfalab/icons-glyph/PlusMediumMIcon';
+import { PlusSIcon } from '@alfalab/icons-glyph/PlusSIcon';
 
 import defaultColors from './default.module.css';
 import styles from './index.module.css';
@@ -48,13 +50,16 @@ export const Steppers: React.FC<SteppersProps> = ({
     const decButtonDisabled = disabled || value <= min;
     const incButtonDisabled = disabled || value >= max;
 
+    const MinusIconComponent = size === 40 ? MinusSIcon : MinusMIcon;
+    const PlusIconComponent = size === 40 ? PlusSIcon : PlusMediumMIcon;
+
     return (
         <div className={cn(styles.component, className)}>
             <IconButton
                 colors={colors}
                 disabled={decButtonDisabled}
                 className={styles.button}
-                icon={<MinusMIcon {...(size === 40 && { width: 16, height: 16 })} />}
+                icon={<MinusIconComponent />}
                 aria-label='уменьшить'
                 onMouseDown={preventDefault}
                 onClick={onDecrement}
@@ -66,7 +71,7 @@ export const Steppers: React.FC<SteppersProps> = ({
                 colors={colors}
                 disabled={incButtonDisabled}
                 className={styles.button}
-                icon={<PlusMediumMIcon {...(size === 40 && { width: 16, height: 16 })} />}
+                icon={<PlusIconComponent />}
                 aria-label='увеличить'
                 onMouseDown={preventDefault}
                 onClick={onIncrement}
