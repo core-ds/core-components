a8c71fa27c48cfaaa09ba5d4e306ba4d52227c2a
Mikhail Lukin <mishalykin@mail.ru>
2024-12-26 15:50:26 +0700

    feat(vars): fix after code review

--- tools/dark-colors-generator.js
+++ tools/dark-colors-generator.js
@@ -3,92 +3,87 @@ const fs = require('fs');
 
 /**
  * Скрипт предназначен для генерации css файлов с переопределением переменных для темной темы
- * Обрабатывает файлы начинающиеся с "colors-"
+ * Обрабатывает файлы которые начинаются с "colors-"
  * Скрипт автоматически запустится при обновлении ui-primitives
+ * Исключены файлы [colors-bluetint.css, colors-indigo.css] => эти палитры обработаны в themes/src/dark.css
+ *
+ * PR https://github.com/core-ds/core-components/pull/1514
  */
 
-const processCssFile = (filePath, outputFilePath) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Ошибка при чтении файла:', err);
-            return;
-        }
-
-        // Регулярное выражение для поиска переменных, содержащих "light"
-        const lightVariablesRegex = /--color-light-[\w-]+:/g;
-
-        // Фильтрация строк, соответствующих переменным с "light"
-        const filteredData = data.match(lightVariablesRegex);
-
-        if (filteredData) {
-            // Обработка отфильтрованных данных: заменяем "light" на "dark" и формируем новую переменную с var()
-            const newCssContent =
-                ':root {\n' +
-                filteredData
-                    .map((variable) => {
-                        // Извлекаем название переменной (без значения)
-                        const varName = variable.split(':')[0].trim();
-
-                        // Заменяем "light" на "dark" и формируем строку вида var(--color-dark-...)
-                        const newVarName = varName.replace('light', 'dark');
-
-                        if (data.includes(newVarName)) {
-                            return `\t${varName}: var(${newVarName});`;
-                        }
-                        return null;
-                    })
-                    // Фильтруем значения у которых нет парного "dark" токена
-                    .filter((variable) => variable !== null)
-                    .join('\n') +
-                '\n}';
-
-            if (!newCssContent.match(lightVariablesRegex)) {
-                console.log(
-                    `Файл ${outputFilePath} не будет записан, так как не содержит парных переменных`,
-                );
-                return;
-            }
-
-            // Запись отфильтрованных данных в новый файл
-            fs.writeFile(outputFilePath, newCssContent, (err) => {
-                if (err) {
-                    console.error('Ошибка при записи в файл:', err);
-                } else {
-                    console.log('Переменные записаны в файл:', outputFilePath);
-                }
-            });
-        } else {
-            console.log(`Не найдено переменных "light" в файле: ${filePath}`);
-        }
-    });
-};
-
 const varsDir = './packages/vars/src';
-fs.readdir(varsDir, (err, files) => {
-    if (err) {
-        console.error('Ошибка при чтении папки:', err);
+const excludedFiles = ['colors-bluetint.css', 'colors-indigo.css'];
+
+const files = fs.readdirSync(varsDir);
+
+// Фильтруем CSS файлы, начинающиеся с "colors"
+const colorCssFiles = files.filter((file) => file.startsWith('colors') && file.endsWith('.css'));
+
+if (colorCssFiles.length === 0) {
+    console.log('[!] Нет файлов, начинающихся с "colors" в папке.');
+    return;
+}
+
+// Обрабатываем отфильтрованные исходные файлы
+colorCssFiles.forEach((file) => {
+    const filePath = path.join(varsDir, file);
+
+    // Отбрасываем уже сгенерированные файлы
+    if (file.includes('-dark.css')) {
         return;
     }
 
-    // Фильтруем только CSS файлы, начинающиеся с "color"
-    const colorCssFiles = files.filter(
-        (file) => file.startsWith('colors') && file.endsWith('.css'),
-    );
-
-    if (colorCssFiles.length === 0) {
-        console.log('Нет файлов, начинающихся с "color" в папке.');
+    // Отбрасываем фалы исключения
+    if (excludedFiles.includes(file)) {
+        console.log(`Пропуск файла: ${filePath}`);
         return;
     }
 
-    // Обрабатываем исходные файлы
-    colorCssFiles.forEach((file, _, array) => {
-        const filePath = path.join(varsDir, file);
+    const outputFilePath = path.join(varsDir, file.replace('.css', '-dark.css'));
 
-        if (file.includes('-dark.css')) {
-            return;
-        }
+    const fileContent = fs.readFileSync(filePath, 'utf8');
 
-        const outputFilePath = path.join(varsDir, file.replace('.css', '-dark.css'));
-        processCssFile(filePath, outputFilePath);
-    });
+    // Регулярное выражение для поиска переменных, содержащих "light"
+    const lightVariablesRegex = /--color-light-[\w-]+:/g;
+
+    // Фильтрация строк, соответствующих переменным с "light"
+    const filteredData = fileContent.match(lightVariablesRegex);
+
+    if (!filteredData) {
+        console.log(`[!] Не найдено переменных "light" в файле: ${filePath}`);
+        return;
+    }
+
+    // Обработка отфильтрованных данных: заменяем "light" на "dark" и формируем новую переменную с var()
+    const newCssContent =
+        ':root {\n' +
+        filteredData
+            .map((variable) => {
+                // Извлекаем название переменной (без значения)
+                const varName = variable.split(':')[0].trim();
+
+                // Заменяем "light" на "dark" и формируем строку вида var(--color-dark-...)
+                const newVarName = varName.replace('light', 'dark');
+
+                if (fileContent.includes(newVarName)) {
+                    return `\t${varName}: var(${newVarName});`;
+                }
+                return null;
+            })
+            // Фильтруем значения у которых нет парного "dark" токена
+            .filter((variable) => variable !== null)
+            .join('\n') +
+        '\n}';
+
+    if (!newCssContent.match(lightVariablesRegex)) {
+        console.log(`[!] Отсутствуют парные переменные, пропуск файла: ${outputFilePath}`);
+        return;
+    }
+
+    try {
+        // Запись отфильтрованных данных в новый файл
+        fs.writeFileSync(outputFilePath, newCssContent);
+        console.log('[+] Переменные записаны в файл:', outputFilePath);
+    } catch (err) {
+        console.error('Ошибка при записи в файл:', err);
+    }
 });
