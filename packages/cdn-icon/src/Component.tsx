3c7696bc54a0cd0171d440b6b935919bb2571bb6
spivakev <spivak.e.v@gmail.com>
2025-02-14 17:24:51 +0100

    refactor(cdn-icon): extract useIcon hook, add useSyncExternalStore hook

--- packages/cdn-icon/src/Component.tsx
+++ packages/cdn-icon/src/Component.tsx
@@ -1,5 +1,7 @@
-import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import React, { ReactNode } from 'react';
 import cn from 'classnames';
+
+import { LoadingStatus, useIcon } from './hooks/use-icon';
 
 import styles from './index.module.css';
 
@@ -32,15 +34,6 @@ type CDNIconProps = {
     fallback?: ReactNode;
 };
 
-enum LoadingStatus {
-    INITIAL,
-    SUCCESS,
-    FAILURE,
-}
-
-// Кэшируем загруженные иконки, чтобы предотвратить их повторную загрузку при каждом монтировании
-const cache: Record<string, string> = {};
-
 export const CDNIcon: React.FC<CDNIconProps> = ({
     name,
     color,
@@ -49,55 +42,24 @@ export const CDNIcon: React.FC<CDNIconProps> = ({
     baseUrl = 'https://alfabank.servicecdn.ru/icons',
     fallback,
 }) => {
-    const url = `${baseUrl}/${name}.svg`;
+    const [icon, status] = useIcon(`${baseUrl}/${name}.svg`);
 
-    const [loadingStatus, setLoadingStatus] = useState<LoadingStatus>(LoadingStatus.INITIAL);
-    const [icon, setIcon] = useState(cache[url]);
-    const prevIconName = useRef(name);
-
-    const monoIcon = !name.includes('_color');
-
-    useEffect(() => {
-        if (cache[url]) {
-            if (prevIconName.current !== name) {
-                setIcon(cache[url]);
-            }
-
-            return undefined;
-        }
-
-        const xhr = new XMLHttpRequest();
-
-        xhr.open('GET', url);
-        xhr.send();
-        xhr.onload = function onload() {
-            setLoadingStatus(LoadingStatus.SUCCESS);
-            const svg = xhr.response;
-
-            if (svg.startsWith('<svg')) {
-                cache[url] = svg;
-
-                setIcon(svg);
-            }
-        };
-
-        xhr.onerror = function onError() {
-            setLoadingStatus(LoadingStatus.FAILURE);
-        };
-
-        return () => xhr.abort();
-    }, [url, icon, name]);
+    const isMonoIcon = !name.includes('_color');
 
     return (
         <span
             style={{ color }}
             className={cn('cc-cdn-icon', styles.component, className, {
-                [styles.parentColor]: monoIcon,
+                [styles.parentColor]: isMonoIcon,
             })}
             data-test-id={dataTestId}
-            {...(loadingStatus === LoadingStatus.FAILURE
-                ? { children: fallback }
-                : { dangerouslySetInnerHTML: { __html: icon } })}
-        />
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                status === LoadingStatus.SUCCESS ? { __html: icon! } : undefined
+            }
+        >
+            {status === LoadingStatus.FAILURE ? fallback : undefined}
+        </span>
     );
 };
