e745ef204455256a533a1ceb26bbafd3e44c442e
Dmitrii Makarkin <makarkin.dv.1990@gmail.com>
2025-02-19 19:31:18 +0300

    fix: added 'email' input type to avoid setSelectionRange issue

---
+++ .changeset/orange-roses-unite.md
@@ -0,0 +1,5 @@
+---
+'@alfalab/core-components-input': patch
+---
+
+Added 'email' input type to avoid setSelectionRange issue
--- packages/input/src/components/base-input/Component.tsx
+++ packages/input/src/components/base-input/Component.tsx
@@ -223,7 +223,7 @@ const SIZE_TO_CLASSNAME_MAP = {
     72: 'size-72',
 };
 
-const inputTypesForSelectionRange = ['password', 'search', 'tel', 'text', 'url'];
+const inputTypesForSelectionRange = ['password', 'search', 'tel', 'text', 'url', 'email'];
 
 export const BaseInput = React.forwardRef<HTMLInputElement, BaseInputProps>(
     (
