4416b152c9071e59a6b024838d01ee435951b6e6
Elena Spivak <37150036+spivakev@users.noreply.github.com>
2025-02-28 19:11:43 +0700

    fix(cdn-icon): fix icon update when name changes (#1558)
    * fix(cdn-icon): fix icon update when name changes
    
    * refactor(cdn-icon): extract useIcon hook, add useSyncExternalStore hook
    
    * fix(cdn-icon): add core-components-shared dependency
    
    * chore(cdn-icon): fix quick-vans-return.md
    
    * chore(cdn-icon): fix quick-vans-return.md
    
    * fix: update dependencies
    
    ---------
    
    Co-authored-by: Elena Spivak <e.spivak@slash.com>
    Co-authored-by: Mikhail Lukin <mishalykin@mail.ru>
    
    (cherry picked from commit 202249e4191bdf50fcf47ff3640e75552b08a8f3)

---
+++ packages/cdn-icon/src/hooks/use-icon.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState, useSyncExternalStore } from 'react';
+
+import { hasOwnProperty, noop } from '@alfalab/core-components-shared';
+
+type Listener = () => void;
+
+export enum LoadingStatus {
+    INITIAL,
+    SUCCESS,
+    FAILURE,
+}
+
+// Кэшируем загруженные иконки, чтобы предотвратить их повторную загрузку при каждом монтировании
+let cache: Record<string, string | undefined> = {};
+let listeners: Listener[] = [];
+
+const iconsStore = {
+    set(url: string, icon: string) {
+        cache = { ...cache, [url]: icon };
+
+        listeners.forEach((listener) => {
+            listener();
+        });
+    },
+    has(url: string) {
+        return hasOwnProperty(cache, url);
+    },
+    subscribe(listener: Listener) {
+        listeners = [...listeners, listener];
+
+        return () => {
+            listeners = listeners.filter((l) => l !== listener);
+        };
+    },
+    getSnapshot() {
+        return cache;
+    },
+};
+
+export function useIcon(url: string): [icon: string | undefined, status: LoadingStatus] {
+    const icons = useSyncExternalStore(iconsStore.subscribe, iconsStore.getSnapshot);
+    const [loadingStatus, setLoadingStatus] = useState(LoadingStatus.INITIAL);
+
+    useEffect(() => {
+        if (iconsStore.has(url)) {
+            setLoadingStatus(LoadingStatus.SUCCESS);
+
+            return noop;
+        }
+
+        setLoadingStatus(LoadingStatus.INITIAL);
+
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('GET', url);
+        xhr.send();
+        xhr.onload = function onload() {
+            const svg = xhr.response;
+
+            if (typeof svg === 'string' && svg.startsWith('<svg')) {
+                iconsStore.set(url, svg);
+            }
+            setLoadingStatus(LoadingStatus.SUCCESS);
+        };
+
+        xhr.onerror = function onError() {
+            setLoadingStatus(LoadingStatus.FAILURE);
+        };
+
+        return () => xhr.abort();
+    }, [url]);
+
+    return [icons[url], loadingStatus];
+}
