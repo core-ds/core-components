f460f24e08946cacf8ce4a7d3ae8272f8f4e9116
hextion <100ishundred@gmail.com>
2025-03-14 17:02:43 +0300

    feat(typography): added Alfa Interface Sans font support

--- packages/typography/src/title-mobile/component.tsx
+++ packages/typography/src/title-mobile/component.tsx
@@ -3,15 +3,14 @@ import React, { forwardRef } from 'react';
 import { TitleBase, TitleProps } from '../title/component';
 
 import commonStyles from '../title/common.module.css';
+import alfasansStyles from './alfasans-index.module.css';
 import styles from './index.module.css';
 
+const allStyles =
+    process.env.NODE_ENV === 'test'
+        ? commonStyles
+        : { ...commonStyles, ...styles, ...alfasansStyles };
+
 export const TitleMobile = forwardRef<HTMLHeadingElement | HTMLDivElement, TitleProps>(
-    (props, ref) => (
-        <TitleBase
-            {...props}
-            styles={Object.assign(commonStyles, styles)}
-            ref={ref}
-            platform='mobile'
-        />
-    ),
+    (props, ref) => <TitleBase {...props} styles={allStyles} ref={ref} platform='mobile' />,
 );
---
+++ tools/postcss/postcss-subtract-mixin.cjs
@@ -0,0 +1,58 @@
+const assert = require('node:assert/strict');
+const crypto = require('node:crypto');
+
+function sha256(str) {
+    return crypto.createHash('sha256').update(str, 'utf8').digest('hex');
+}
+
+const postcssSubtractMixin = () => ({
+    postcssPlugin: 'postcss-subtract-mixin-plugin',
+    prepare: () => {
+        const store = [];
+
+        return {
+            Once(root, { AtRule, Rule }) {
+                root.walkAtRules((atrule) => {
+                    if (atrule.name === 'subtract-mixin') {
+                        const mixinNamess = atrule.params.split(',').map((name) => name.trim());
+
+                        const rules = mixinNamess.map(
+                            (mixinName) =>
+                                new Rule({
+                                    selector: `.${sha256(`${mixinName}-${Math.random()}`)}`,
+                                    nodes: [new AtRule({ name: 'mixin', params: mixinName })],
+                                }),
+                        );
+
+                        atrule.replaceWith(rules);
+                        store.push(rules);
+                    }
+                });
+            },
+            OnceExit: () => {
+                store.forEach((rules) => {
+                    rules.forEach((rule) => {
+                        // every rule must have declarations only
+                        assert(rule.nodes.every((node) => node.type == 'decl'));
+                    });
+                    const [source, ...rest] = rules;
+                    const subtracts = rest.map(({ nodes }) => nodes).reduce((a, b) => a.concat(b));
+
+                    const result = source.nodes.filter(
+                        (sourceNode) =>
+                            !subtracts.some(
+                                (node) =>
+                                    node.prop === sourceNode.prop &&
+                                    node.value === sourceNode.value,
+                            ),
+                    );
+
+                    source.parent.append(result);
+                    rules.forEach((rule) => rule.remove());
+                });
+            },
+        };
+    },
+});
+
+module.exports = postcssSubtractMixin;
