8a2e13cbf92d8c846a0338503f0a0cdb1ce97a37
Pavel German <pvgerman@alfabank.ru>
2025-03-21 17:20:25 +1000

    fix(amount-input): fixed behavior of deleting values ​​in input

--- packages/amount-input/src/Component.tsx
+++ packages/amount-input/src/Component.tsx
@@ -4,7 +4,7 @@ import cn from 'classnames';
 import { Input, InputProps } from '@alfalab/core-components-input';
 import { withSuffix } from '@alfalab/core-components-with-suffix';
 import { CurrencyCodes } from '@alfalab/data';
-import { formatAmount, THINSP } from '@alfalab/utils';
+import { formatAmount, MMSP, THINSP } from '@alfalab/utils';
 
 import {
     getAmountValueFromStr,
@@ -199,12 +199,17 @@ export const AmountInput = forwardRef<HTMLInputElement, AmountInputProps>(
                 `(^${positiveOnly ? '' : '-?'}[0-9]{0,${integerLength}}(,([0-9]+)?)?$|^\\s*$)`,
             ).test(enteredValue);
 
+            let caret = input.selectionStart as number;
+
             if (isCorrectEnteredValue) {
+                if (inputValue[caret] === MMSP) {
+                    enteredValue = enteredValue.slice(0, caret - 1) + enteredValue.slice(caret);
+                    caret -= 1;
+                }
+
                 const newFormattedValue = getFormattedValue(enteredValue, currency, minority);
 
                 if (newFormattedValue === inputValue) {
-                    const caret = input.selectionStart;
-
                     window.requestAnimationFrame(() => {
                         input.selectionStart = caret;
                         input.selectionEnd = caret;
@@ -230,7 +235,7 @@ export const AmountInput = forwardRef<HTMLInputElement, AmountInputProps>(
                     }
 
                     const diff = newFormattedValue.length - notFormattedEnteredValueLength;
-                    const caret = (input.selectionStart as number) + diff;
+                    caret += diff;
 
                     window.requestAnimationFrame(() => {
                         input.selectionStart = caret;
@@ -245,7 +250,7 @@ export const AmountInput = forwardRef<HTMLInputElement, AmountInputProps>(
                 });
             } else {
                 // Не двигаем каретку когда вставляется невалидный символ
-                const caret = (input.selectionStart as number) - 1;
+                caret -= 1;
 
                 window.requestAnimationFrame(() => {
                     input.selectionStart = caret;
