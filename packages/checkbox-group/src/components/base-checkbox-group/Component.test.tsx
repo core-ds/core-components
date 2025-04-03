9f055e3de3903b7fc1bdd3192c4e5e064908a679
Vadim Kalushko <vakalushko.job@yandex.ru>
2025-04-03 23:02:56 +0500

    fix(checkbox-group): обернуть компонент BaseCheckboxGroup в forwardRef [DS-9475]

--- packages/checkbox-group/src/components/base-checkbox-group/Component.test.tsx
+++ packages/checkbox-group/src/components/base-checkbox-group/Component.test.tsx
@@ -1,10 +1,18 @@
-import { forwardRef } from 'react';
+import React, { createRef } from 'react';
+import { render } from '@testing-library/react';
 import { BaseCheckboxGroup } from './Component';
 
 describe('BaseCheckboxGroup', () => {
-    it('should be wrapped in forwardRef', () => {
-        const forwardRefType = forwardRef(() => null).$$typeof;
+    it('should pass the ref to the DOM element', () => {
+        const styles = { mockClassName: 'mockStyles' };
+        const passedRef = createRef<HTMLDivElement>();
 
-        expect(BaseCheckboxGroup.$$typeof).toEqual(forwardRefType);
+        render(
+            <BaseCheckboxGroup ref={passedRef} styles={styles}>
+                <div>Test Child</div>
+            </BaseCheckboxGroup>,
+        );
+
+        expect(passedRef.current).not.toBeNull();
     });
 });
