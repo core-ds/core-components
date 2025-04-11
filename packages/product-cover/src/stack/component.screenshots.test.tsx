36977af289760aa0455ee35b8a21def3dd0daf68
denisx <github.com@denisx.com>
2025-04-11 12:56:39 +0300

    fix: product-cover: исправлено название пропса numberOfCards (#1489)

--- packages/product-cover/src/stack/component.screenshots.test.tsx
+++ packages/product-cover/src/stack/component.screenshots.test.tsx
@@ -26,7 +26,7 @@ describe(
                         secondCard: {
                             cardNumber: 1234000000001234,
                         },
-                        numberOfСards: 2,
+                        numberOfCards: 2,
                     },
                 }),
             ],
@@ -42,7 +42,7 @@ describe(
                             cardNumber: 1234000000001234,
                             shadow: '2px 2px 2px 0px rgba(0, 0, 0, 0.20)',
                         },
-                        numberOfСards: 2,
+                        numberOfCards: 2,
                     },
                 }),
             ],
@@ -60,7 +60,7 @@ describe(
                         secondCard: {
                             cardNumber: 1234000000001234,
                         },
-                        numberOfСards: 2,
+                        numberOfCards: 2,
                     },
                 }),
             ],
