6856d651af15eaf57faa42e725782e8bdeb1037a
Vadim Kalushko <vakalushko.job@yandex.ru>
2025-04-14 12:55:50 +0500

    CodeReview

--- packages/custom-button/src/index.ts
+++ packages/custom-button/src/index.ts
@@ -1,4 +1,2 @@
 export { CustomButton } from './Component';
-export { CustomButtonDesktop } from './desktop';
-export { CustomButtonMobile } from './mobile';
 export { CustomButtonProps, ComponentProps } from './types/props';
