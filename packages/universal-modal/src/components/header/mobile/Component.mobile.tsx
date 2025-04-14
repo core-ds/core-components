f49e0840136ab53870ab6d841207f28bff3803ae
fulcanellee <45999900+fulcanellee@users.noreply.github.com>
2025-04-14 16:19:41 +0700

    fix(universal-modal): fix mobile modal closing (#1670)

--- packages/universal-modal/src/components/header/mobile/Component.mobile.tsx
+++ packages/universal-modal/src/components/header/mobile/Component.mobile.tsx
@@ -22,13 +22,27 @@ export const HeaderMobile: FC<HeaderMobileProps> = ({
     sticky,
     dataTestId,
     bottomAddonsClassName,
+    onClose,
     ...restProps
 }) => {
-    const { setHasHeader, headerHighlighted, componentRef } = useContext(ModalContext);
+    const {
+        setHasHeader,
+        headerHighlighted,
+        componentRef,
+        onClose: handleCloseByContext,
+    } = useContext(ModalContext);
 
     const titleRef = useRef<HTMLDivElement>(null);
 
     const hasContent = Boolean(title || children || restProps.bottomAddons);
+
+    const handleClose: NavigationBarPrivateProps['onClose'] = (...args) => {
+        if (onClose) {
+            return onClose(...args);
+        }
+
+        return handleCloseByContext(...args);
+    };
 
     useEffect(() => {
         setHasHeader(true);
@@ -52,6 +66,7 @@ export const HeaderMobile: FC<HeaderMobileProps> = ({
             scrollableParentRef={componentRef}
             titleRef={titleRef}
             addonClassName={mobileStyles.addon}
+            onClose={handleClose}
         >
             {children}
         </NavigationBarPrivate>
