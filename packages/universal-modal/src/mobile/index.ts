1e03bad2f32093c5c26af3572293adbde822bc3b
Mikhail Lukin <mishalykin@mail.ru>
2025-04-14 18:42:47 +0700

    fix(universal-modal): get rid of cross button from mobile modal

--- packages/universal-modal/src/mobile/index.ts
+++ packages/universal-modal/src/mobile/index.ts
@@ -10,5 +10,4 @@ export type { FooterMobileProps } from '../components/footer';
 export { ContentMobile } from '../components/content';
 export type { ContentMobileProps } from '../components/content';
 
-export { CrossButtonMobile } from './components/buttons/cross-button';
 export { ArrowButtonMobile } from './components/buttons/arrow-button';
