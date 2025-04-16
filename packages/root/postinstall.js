c5f320662b59599c201e88c0247874d04f8aea99
Salavat Gizatov <100ishundred@gmail.com>
2025-04-16 11:20:28 +0300

    fix: postinstall (#22)

--- packages/root/postinstall.js
+++ packages/root/postinstall.js
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
 const path = require('path');
+const resolveFrom = require('resolve-from');
 const pkgJSON = require('./package.json');
 
 Object.keys(pkgJSON.dependencies)
     .filter((pkg) => pkg.startsWith('@balafla/core-components-'))
     .forEach((pkg) => {
         const targetPath = path.join(__dirname, pkg.replace('@balafla/core-components-', ''));
-        const pkgPath = path.dirname(require.resolve(`${pkg}/package.json`));
+        const pkgPath = path.dirname(resolveFrom(__dirname, `${pkg}/package.json`));
 
         if (fs.existsSync(targetPath)) {
             fs.rmSync(targetPath, { recursive: true, force: true, maxRetries: 3 });
