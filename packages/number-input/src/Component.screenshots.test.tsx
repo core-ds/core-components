a9fb775dbc07e43181f0b7fb755e55362f430111
Mikhail Lukin <mishalykin@mail.ru>
2025-04-16 15:34:47 +0700

    feat(number-input): refactor

--- packages/number-input/src/Component.screenshots.test.tsx
+++ packages/number-input/src/Component.screenshots.test.tsx
@@ -1,4 +1,15 @@
-import { createPreview } from '../../screenshot-utils';
+import {
+    createPreview,
+    createSpriteStorybookUrl,
+    setupScreenshotTesting,
+} from '../../screenshot-utils';
+
+const screenshotTesting = setupScreenshotTesting({
+    it,
+    beforeAll,
+    afterAll,
+    expect,
+});
 
 describe('NumberInput ', () =>
     createPreview(
@@ -16,3 +27,59 @@ describe('NumberInput ', () =>
             viewport: { width: 1024, height: 600 },
         },
     ));
+
+describe('NumberInput Stepper', () =>
+    createPreview(
+        {
+            componentName: 'NumberInput',
+            knobs: {
+                label: 'Число',
+                value: 1234,
+                size: 56,
+                block: true,
+                step: 1,
+            },
+        },
+        'padding: 0 270px;width:800px;transform:scale(2.1)',
+        {
+            viewport: { width: 1024, height: 600 },
+        },
+    ));
+
+describe(
+    'NumberInput | size',
+    screenshotTesting({
+        cases: [
+            [
+                'sprite',
+                createSpriteStorybookUrl({
+                    componentName: 'NumberInput',
+                    knobs: {
+                        label: 'Число',
+                        value: 1234,
+                        size: [40, 48, 56],
+                        block: true,
+                        step: 1,
+                        colors: ['default'],
+                    },
+                    size: { width: 300, height: 70 },
+                }),
+            ],
+            [
+                'sprite',
+                createSpriteStorybookUrl({
+                    componentName: 'NumberInput',
+                    knobs: {
+                        label: 'Число',
+                        value: 1234,
+                        size: [40, 48, 56],
+                        block: true,
+                        step: 1,
+                        colors: ['inverted'],
+                    },
+                    size: { width: 300, height: 70 },
+                }),
+            ],
+        ],
+    }),
+);
