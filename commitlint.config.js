0f2c0cb83b7adfa2746463f29ae7110384b6259e
hextion <100ishundred@gmail.com>
2025-04-18 14:20:52 +0300

    fix: no lerna usage

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,25 +1,31 @@
 const assert = require('assert');
-const {
-    utils: { getPackages },
-} = require('@commitlint/config-lerna-scopes');
+const { getPackages } = require('@manypkg/get-packages');
 
-const PACKAGE_NAME_PREFIX_REG_EXP = /^core-components-/;
+const PACKAGE_NAME_PREFIX_REG_EXP = /^@alfalab\/core-components-/;
 
-function getScopes(packages) {
-    return packages.map((packageName) => {
-        if (packageName === 'core-components') {
+/**
+ * @param {import('@manypkg/get-packages').Packages} packages
+ * @returns {string[]}
+ */
+function getScopes({ packages }) {
+    return packages.map(({ packageJson: { name } }) => {
+        if (name === '@alfalab/core-components') {
             return 'root';
         }
 
-        assert(PACKAGE_NAME_PREFIX_REG_EXP.test(packageName));
+        assert(PACKAGE_NAME_PREFIX_REG_EXP.test(name), name);
 
-        return packageName.replace(PACKAGE_NAME_PREFIX_REG_EXP, '');
+        return name.replace(PACKAGE_NAME_PREFIX_REG_EXP, '');
     });
 }
 
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'scope-enum': async (ctx) => [2, 'always', getScopes(await getPackages(ctx))],
+        'scope-enum': async (ctx) => [
+            2,
+            'always',
+            getScopes(await getPackages(ctx?.cwd ?? process.cwd())),
+        ],
     },
 };
