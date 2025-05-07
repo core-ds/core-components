9bb4ae9e98c9cea68764017cd73648c758284f9f
hextion <100ishundred@gmail.com>
2025-05-07 19:21:55 +0300

    refactor: replaced resolve-from with resolve

--- packages/root/postinstall.js
+++ packages/root/postinstall.js
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
 const path = require('path');
-const resolveFrom = require('resolve-from');
-const pkgJSON = require('./package.json');
+const resolve = require('resolve/sync');
+const pkg = require('./package.json');
 
-Object.keys(pkgJSON.dependencies)
-    .filter((pkg) => pkg.startsWith('@alfalab/core-components-'))
-    .forEach((pkg) => {
-        const targetPath = path.join(__dirname, pkg.replace('@alfalab/core-components-', ''));
-        const pkgPath = path.dirname(resolveFrom(__dirname, `${pkg}/package.json`));
+const CORE_COMPONENTS_REGEXP = /^@alfalab\/core-components-/;
+
+Object.keys(pkg.dependencies)
+    .filter((name) => CORE_COMPONENTS_REGEXP.test(name))
+    .forEach((name) => {
+        const targetPath = path.join(__dirname, name.replace(CORE_COMPONENTS_REGEXP, ''));
+        const pkgPath = path.dirname(resolve(`${name}/package.json`, { basedir: __dirname }));
 
         if (fs.existsSync(targetPath)) {
             fs.rmSync(targetPath, { recursive: true, force: true, maxRetries: 3 });
