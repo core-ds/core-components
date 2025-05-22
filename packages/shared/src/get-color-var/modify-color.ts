892884acd2e350cfd1bc4516e5d85c6ba14f6525
Artem <38911256+temss-front@users.noreply.github.com>
2025-05-22 12:49:32 +0300

    feat(shared): update by pr comment

--- packages/shared/src/get-color-var/modify-color.ts
+++ packages/shared/src/get-color-var/modify-color.ts
@@ -4,17 +4,11 @@
  * @returns Цвет в формате "rgba(r, g, b, a)" для использования в CSS
  */
 export function modifyColor(color: string): string {
-    // Проверяем формат #AARRGGBB (8 символов с решеткой)
-    if (color.startsWith('#') && color.length === 9) {
-        // Извлекаем компоненты цвета
-        const alpha = parseInt(color.slice(1, 3), 16) / 255; // Преобразуем A в десятичное значение от 0 до 1
-        const red = parseInt(color.slice(3, 5), 16);
-        const green = parseInt(color.slice(5, 7), 16);
-        const blue = parseInt(color.slice(7, 9), 16);
-
-        // Возвращаем цвет в формате rgba
-        return `rgba(${red}, ${green}, ${blue}, ${alpha.toFixed(2)})`;
+    if (!/^#([0-9a-fA-F]){8}$/.test(color)) {
+        return color;
     }
 
-    return color;
+    const [alpha, red, green, blue] = color.match(/([0-9a-fA-F]{2})/g)!.map((s) => parseInt(s, 16));
+
+    return `rgba(${red}, ${green}, ${blue}, ${(alpha / 255).toFixed(2)})`;
 }
