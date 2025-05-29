6d30fa4ba44db26d902447565e8857b05b027e84
hextion <100ishundred@gmail.com>
2025-05-29 11:25:36 +0300

    fix: lint script

--- bin/lint-js.sh
+++ bin/lint-js.sh
@@ -3,6 +3,7 @@
 lerna exec \
     --no-bail \
     --stream \
+    --ignore @alfalab/core-components-codemod \
     --ignore @alfalab/core-components-themes \
     --ignore @alfalab/core-components-vars \
     -- eslint src --ext .ts,.tsx,.js,.jsx --max-warnings 0 --config $(pwd)/.eslintrc.cjs
