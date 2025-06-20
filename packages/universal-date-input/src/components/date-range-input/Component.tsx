137396a7cf0a0ae51f0b63b8a2a30bb9bf25e222
Pavel Kondratyev <58857483+Haskiro@users.noreply.github.com>
2025-06-20 10:28:47 +0300

    fix(universal-date-input): fix onBlur handler in date-range-input (#1723)

--- packages/universal-date-input/src/components/date-range-input/Component.tsx
+++ packages/universal-date-input/src/components/date-range-input/Component.tsx
@@ -179,8 +179,6 @@ export const DateRangeInput = forwardRef<HTMLInputElement, InnerDateRangeInputPr
         };
 
         const handleBlur = (event: FocusEvent<HTMLInputElement>) => {
-            if (open) return;
-
             onBlur?.(event);
 
             if (autoCorrection) {
