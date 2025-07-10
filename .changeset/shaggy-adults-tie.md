c84b8dabead4401f2a917f311f3372d25e8ddb6f
Vadim Kalushko <vakalushko.job@yandex.ru>
2025-07-10 22:41:59 +0500

    feat(checkbox-group): Компонент был обернут в forwardRef

---
+++ .changeset/shaggy-adults-tie.md
@@ -0,0 +1,5 @@
+---
+'@alfalab/core-components-checkbox-group': minor
+---
+
+- Компонент был обернут в forwardRef
--- packages/checkbox-group/src/mobile/Component.mobile.tsx
+++ packages/checkbox-group/src/mobile/Component.mobile.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { forwardRef } from 'react';
 
 import { BaseCheckboxGroup, BaseCheckboxGroupProps } from '../components/base-checkbox-group';
 
@@ -6,6 +6,6 @@ import styles from './mobile.module.css';
 
 export type CheckboxGroupMobileProps = Omit<BaseCheckboxGroupProps, 'styles'>;
 
-export const CheckboxGroupMobile: FC<CheckboxGroupMobileProps> = (props) => (
-    <BaseCheckboxGroup {...props} styles={styles} />
+export const CheckboxGroupMobile = forwardRef<HTMLDivElement, CheckboxGroupMobileProps>(
+    (props, ref) => <BaseCheckboxGroup {...props} styles={styles} ref={ref} />,
 );
