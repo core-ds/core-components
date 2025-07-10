c8e728539e99f617037c8a53510cc4200b2bde1c
Vadim Kalushko <vakalushko.job@yandex.ru>
2025-07-10 17:50:16 +0500

    Добавляем инверсию цвета в BottomSheet

---
+++ .changeset/wicked-bugs-help.md
@@ -0,0 +1,15 @@
+---
+'@alfalab/core-components-bottom-sheet': minor
+'@alfalab/core-components-navigation-bar-private': minor
+'@alfalab/core-components-collapse': patch
+'@alfalab/core-components-input-autocomplete': patch
+'@alfalab/core-components-modal': patch
+'@alfalab/core-components-picker-button': patch
+'@alfalab/core-components-side-panel': patch
+'@alfalab/core-components-themes': patch
+'@alfalab/core-components-vars': patch
+---
+
+- Добавлена инверсия BottomSheet через props `colors`
+- Добавлена возможность пробрасывать colors в `NavigationBar` для того, чтобы динамически изменять тему элементов `hasCloser` и `hasBacker`
+- Добавлен проброс prop `colors` через компонент `Collapse` в `Link`
--- packages/bottom-sheet/src/components/footer/Component.tsx
+++ packages/bottom-sheet/src/components/footer/Component.tsx
@@ -3,7 +3,12 @@ import cn from 'classnames';
 
 import { BaseModalContext } from '@alfalab/core-components-base-modal';
 
+import { ColorType } from '../../types';
+import { getColorStyles } from '../../utils';
+
+import defaultColors from './default.module.css';
 import styles from './index.module.css';
+import invertedColors from './inverted.module.css';
 
 export type FooterProps = {
     /**
@@ -25,20 +30,33 @@ export type FooterProps = {
      * Идентификатор для систем автоматизированного тестирования
      */
     dataTestId?: string;
+
+    /**
+     * Набор цветов для компонента
+     */
+    colors?: ColorType;
 };
 
-export const Footer: FC<FooterProps> = ({ children, className, sticky, dataTestId }) => {
+export const Footer: FC<FooterProps> = ({
+    children,
+    className,
+    colors = 'default',
+    sticky,
+    dataTestId,
+}) => {
     const { footerHighlighted, setHasFooter } = useContext(BaseModalContext);
 
     useEffect(() => {
         setHasFooter(true);
     }, [setHasFooter]);
 
+    const colorStyle = getColorStyles(colors, defaultColors, invertedColors);
+
     return (
         <div
-            className={cn(styles.footer, className, {
+            className={cn(styles.footer, colorStyle.hasContent, className, {
                 [styles.sticky]: sticky,
-                [styles.highlighted]: footerHighlighted && sticky,
+                [colorStyle.highlighted]: footerHighlighted && sticky,
             })}
             data-test-id={dataTestId}
         >
