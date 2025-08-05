14b76b6ddc52381f66bbe33773d78f23388fb5ec
u_m1mkt <sowtame@gmail.com>
2025-08-05 13:56:18 +0300

    feat(deploy_kube_feature): add new component

---
+++ packages/image/src/docs/Component.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { CoreConfigContext } from '@alfalab/core-components-config';
+
+import { Image } from '..';
+
+const meta: Meta<typeof Image> = {
+    title: 'Components/Image',
+    component: Image,
+    id: 'Image',
+};
+
+type Story = StoryObj<typeof Image>;
+
+export const image: Story = {
+    name: 'Image',
+    render: () => {
+        return (
+            <CoreConfigContext.Provider
+                value={{
+                    components: {
+                        Image: {
+                            inViewOption: true,
+                            proxyMap: [
+                                {
+                                    from: 'https://web.alfabank.ru/mobile',
+                                    to: 'https://alfaonline.servicecdn.ru/public',
+                                },
+                            ],
+                        },
+                    },
+                }}
+            >
+                <div>1</div>
+                <div style={{ height: '1000px' }}></div>
+                <Image src='https://web-test.alfabank.ru/mobile/s3/static/loyalty/services/travel_300x300.png' />
+                <div style={{ height: '300px' }}></div>
+                <Image src='https://web.alfabank.ru/mobile/s3/static/loyalty/widget/pforfit_492%D1%85469.png' />
+            </CoreConfigContext.Provider>
+        );
+    },
+};
+
+export default meta;
