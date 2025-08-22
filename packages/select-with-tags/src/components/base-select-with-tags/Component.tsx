9bba6a658e83fd356623df89df7a6a177613dd81
hextion <100ishundred@gmail.com>
2025-08-22 13:53:23 +0300

    fix(select-with-tags): gap

--- packages/select-with-tags/src/components/base-select-with-tags/Component.tsx
+++ packages/select-with-tags/src/components/base-select-with-tags/Component.tsx
@@ -224,6 +224,7 @@ export const BaseSelectWithTags = forwardRef<HTMLInputElement, BaseSelectWithTag
                                   },
                               },
                               bottomSheetProps: {
+                                  showFooter: false,
                                   showSwipeMarker: false,
                                   onClose: handleClose,
                                   containerProps: {
