6da960a77343869e4e6a866d6100a0d7b9df8bd4
Vadim Kalushko <vakalushko.job@yandex.ru>
2025-11-01 10:54:27 +0300

    fix(plate): Изменение скруглений [DS-13559] (#1924)
    * Изменение скруглений
    
    * CodeReview

--- packages/plate/src/components/base-plate/Component.tsx
+++ packages/plate/src/components/base-plate/Component.tsx
@@ -166,6 +166,11 @@ export type BasePlateProps = {
      * Основные стили компонента.
      */
     styles?: { [key: string]: string };
+
+    /**
+     * Кастомный радиус скругления (px, rem, em и т.д.)
+     */
+    borderRadius?: string;
 };
 
 /* eslint-disable complexity */
@@ -200,6 +205,7 @@ export const BasePlate = forwardRef<HTMLDivElement, BasePlateProps>(
             styles = {},
             background,
             borderColor,
+            borderRadius,
         },
         ref,
     ) => {
@@ -320,6 +326,7 @@ export const BasePlate = forwardRef<HTMLDivElement, BasePlateProps>(
                 style={{
                     ...setCustomViewColors(),
                     ...setCustomViewRectangleBorderColor(),
+                    ...(borderRadius && { borderRadius }),
                 }}
                 onClick={handleClick}
                 onKeyDown={handleClick}
