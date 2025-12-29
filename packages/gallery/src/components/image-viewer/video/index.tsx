1e03cb274ed472a31551be5b93a2f6525f70218c
Artur <APambukhchyan@alfabank.ru>
2025-12-29 12:16:08 +0300

    fix: by lint

--- packages/gallery/src/components/image-viewer/video/index.tsx
+++ packages/gallery/src/components/image-viewer/video/index.tsx
@@ -9,7 +9,7 @@ import React, {
 } from 'react';
 import cn from 'classnames';
 import type HlsType from 'hls.js/dist/hls.light.mjs';
-import  { type ErrorData, type Events } from 'hls.js/dist/hls.light.mjs';
+import { type ErrorData, type Events } from 'hls.js/dist/hls.light.mjs';
 
 import { Circle } from '@alfalab/core-components-icon-view/circle';
 import PlayCompactMIcon from '@alfalab/icons-glyph/PlayCompactMIcon';
