332b4a699f609cdfe6af382ce534f55a74574b7a
hextion <100ishundred@gmail.com>
2026-01-19 16:23:37 +0300

    feat(toast-plate): added boldTitle prop

---
+++ .changeset/crazy-hoops-double.md
@@ -0,0 +1,8 @@
+---
+'@alfalab/core-components': minor
+'@alfalab/core-components-toast-plate': minor
+---
+
+##### ToastPlate{Desktop,Mobile}
+
+- Добавлен проп `boldTitle` для управления жирностью заголовка
--- packages/notification/src/Component.tsx
+++ packages/notification/src/Component.tsx
@@ -238,6 +238,7 @@ export const Notification = forwardRef<HTMLDivElement, NotificationProps>(
                                 role={visible ? 'alert' : undefined}
                                 hasCloser={hasCloser}
                                 onClose={onClose}
+                                boldTitle={true}
                                 {...restProps}
                             >
                                 {children}
