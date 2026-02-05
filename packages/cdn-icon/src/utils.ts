cca80e9a353f6fd96bfc3fd9b279fb972c84d854
Vadim Kalushko <vakalushko.job@yandex.ru>
2026-02-05 11:43:07 +0300

    fix

--- packages/cdn-icon/src/utils.ts
+++ packages/cdn-icon/src/utils.ts
@@ -1,4 +1,8 @@
-export const buildIconUrl = (iconName: string, iconBaseUrl: string): string | null => {
+export const buildIconUrl = (iconName: unknown, iconBaseUrl: string): string | null => {
+    if (typeof iconName !== 'string') {
+        return null;
+    }
+
     const trimmedName = iconName.trim();
     const hasAlphaNumeric = /[a-z0-9]/i.test(trimmedName);
 
