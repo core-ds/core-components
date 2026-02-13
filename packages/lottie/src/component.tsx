8a5fbca9677b7f5255c460b0149543702caff229
hextion <100ishundred@gmail.com>
2026-02-13 11:28:34 +0300

    refactor(lottie): better

--- packages/lottie/src/component.tsx
+++ packages/lottie/src/component.tsx
@@ -1,73 +1,20 @@
-import React, {
-    type CSSProperties,
-    forwardRef,
-    type ReactNode,
-    useImperativeHandle,
-    useRef,
-    useState,
-} from 'react';
+import React, { forwardRef, useImperativeHandle, useRef, useState } from 'react';
 import cn from 'classnames';
-import {
-    type AnimationDirection,
-    type AnimationEventCallback,
-    type AnimationEventName,
-    type AnimationEvents,
-} from 'lottie-web/build/player/lottie_light';
+import { createNanoEvents } from 'nanoevents';
 
 import { noop } from '@alfalab/core-components-shared';
 import { useLayoutEffect_SAFE_FOR_SSR } from '@alfalab/hooks';
 
-import { type AnimationItemListenersMap, useLottie } from './react-lottie';
+import { useLottie } from './react-lottie';
+import {
+    type AnimationDirection,
+    LottieDataState,
+    type LottieEvents,
+    type LottieProps,
+    type LottieRef,
+} from './types';
 
 import styles from './index.module.css';
-
-interface LottieRef {
-    reset(): void;
-    subscribe<T extends Extract<AnimationEventName, 'enterFrame'>>(
-        name: T,
-        callback: AnimationEventCallback<AnimationEvents[T]>,
-    ): () => void;
-}
-
-export interface LottieProps {
-    play?: boolean;
-    speed?: number;
-    initialFrame?: number;
-    startFrame?: number;
-    endFrame?: number;
-    iterations?: number;
-    reverseOnRepeat?: boolean;
-    direction?: AnimationDirection;
-    animation: { path: string; data?: never } | { path?: never; data: unknown };
-    placeholder?: (dataState: LottieDataState.LOADING | LottieDataState.ERROR) => ReactNode;
-    scale: 'fit' | 'fill';
-    size?: Pick<
-        CSSProperties,
-        'width' | 'height' | 'minWidth' | 'minHeight' | 'maxWidth' | 'maxHeight'
-    >;
-    className?: string;
-}
-
-const addListener =
-    <T, U extends keyof T>(name: U, callback: AnimationEventCallback<T[U]>) =>
-    (listeners: AnimationItemListenersMap<T>): AnimationItemListenersMap<T> => ({
-        ...listeners,
-        [name]: [...(listeners[name] ?? []), callback],
-    });
-
-const removeListener =
-    <T, U extends keyof T>(name: U, callback: AnimationEventCallback<T[U]>) =>
-    (listeners: AnimationItemListenersMap<T>): AnimationItemListenersMap<T> => ({
-        ...listeners,
-        [name]: listeners[name]?.filter((c) => c !== callback),
-    });
-
-export enum LottieDataState {
-    OK,
-    ERROR,
-    LOADING,
-    INITIAL,
-}
 
 export const Lottie = forwardRef<LottieRef, LottieProps>(
     (
@@ -78,8 +25,7 @@ export const Lottie = forwardRef<LottieRef, LottieProps>(
             initialFrame: initialFrameFromProps = startFrame,
             endFrame,
             iterations: iterationsFromProps = 0,
-            reverseOnRepeat = false,
-            direction = 1,
+            direction,
             animation: animationData,
             placeholder,
             scale = 'fill',
@@ -104,20 +50,7 @@ export const Lottie = forwardRef<LottieRef, LottieProps>(
         });
         const playCountRef = useRef(animation?.playCount ?? 0);
         const [dataState, setDataState] = useState(LottieDataState.INITIAL);
-        const [listeners, setListeners] = useState<AnimationItemListenersMap<AnimationEvents>>(
-            () => ({
-                DOMLoaded: [
-                    () => {
-                        setDataState(LottieDataState.OK);
-                    },
-                ],
-                data_failed: [
-                    () => {
-                        setDataState(LottieDataState.ERROR);
-                    },
-                ],
-            }),
-        );
+        const [events] = useState(() => createNanoEvents<LottieEvents>());
         let initialFrame = Math.max(startFrame, initialFrameFromProps);
 
         initialFrame =
@@ -128,44 +61,14 @@ export const Lottie = forwardRef<LottieRef, LottieProps>(
             () => ({
                 reset,
                 subscribe(name, callback) {
-                    if (name === 'enterFrame') {
-                        setListeners(addListener(name, callback));
-
-                        return () => {
-                            setListeners(removeListener(name, callback));
-                        };
-                    }
-
-                    return noop;
+                    return events.on(name, callback);
                 },
             }),
-            [reset],
+            [events, reset],
         );
         useImperativeHandle(playCountRef, () => animation?.playCount ?? 0, [animation]);
 
-        // handle listeners
-        useLayoutEffect_SAFE_FOR_SSR(() => {
-            if (animation) {
-                const subscriptions = Object.entries(listeners).flatMap((entry) => {
-                    const [name, callbacks = []] = entry as [
-                        name: AnimationEventName,
-                        callbacks:
-                            | Array<AnimationEventCallback<AnimationEvents[AnimationEventName]>>
-                            | undefined,
-                    ];
-
-                    return callbacks.map((callback) => animation.addEventListener(name, callback));
-                });
-
-                return () => {
-                    subscriptions.forEach((unsubscribe) => unsubscribe());
-                };
-            }
-
-            return noop;
-        }, [animation, listeners]);
-
-        // handle loading
+        // set loading state
         useLayoutEffect_SAFE_FOR_SSR(() => {
             if (animation) {
                 setDataState(animation.isLoaded ? LottieDataState.OK : LottieDataState.LOADING);
@@ -177,7 +80,9 @@ export const Lottie = forwardRef<LottieRef, LottieProps>(
         // handle direction and speed
         useLayoutEffect_SAFE_FOR_SSR(() => {
             if (animation && playCountRef.current < iterations) {
-                animation.setDirection(direction);
+                if (typeof direction === 'number') {
+                    animation.setDirection(direction);
+                }
                 animation.setSpeed(speed);
             }
         }, [animation, direction, iterations, speed]);
@@ -186,46 +91,86 @@ export const Lottie = forwardRef<LottieRef, LottieProps>(
         useLayoutEffect_SAFE_FOR_SSR(() => {
             const playCount = playCountRef.current;
 
-            if (animation && playCount < iterations && play && animation.isPaused) {
+            if (
+                animation &&
+                dataState === LottieDataState.OK &&
+                playCount < iterations &&
+                play &&
+                animation.isPaused
+            ) {
                 if (playCount === 0) {
                     const playFrame = Math.max(animation.currentFrame, initialFrame);
 
-                    animation.goToAndPlay(playFrame, true);
+                    if (playFrame === animation.currentFrame) {
+                        animation.play();
+                    } else {
+                        animation.goToAndPlay(playFrame, true);
+                    }
+
+                    events.emit(playFrame === initialFrame ? 'started' : 'resumed');
                 } else {
                     animation.play();
+                    events.emit('resumed');
                 }
             }
-        }, [animation, initialFrame, iterations, play]);
+        }, [animation, dataState, events, initialFrame, iterations, play]);
 
         // handle pause
         useLayoutEffect_SAFE_FOR_SSR(() => {
-            if (animation && playCountRef.current < iterations && !play && !animation.isPaused) {
+            if (
+                animation &&
+                dataState === LottieDataState.OK &&
+                playCountRef.current < iterations &&
+                !play &&
+                !animation.isPaused
+            ) {
                 animation.pause();
+                events.emit('stopped');
             }
-        }, [animation, iterations, play]);
+        }, [animation, dataState, events, iterations, play]);
 
-        // handle iterations
+        // handle listeners
         useLayoutEffect_SAFE_FOR_SSR(() => {
             if (animation) {
-                return animation.addEventListener('complete', () => {
-                    playCountRef.current += 1;
+                const subscriptions = [
+                    animation.addEventListener('complete', () => {
+                        playCountRef.current += 1;
 
-                    if (playCountRef.current < iterations) {
-                        if (reverseOnRepeat) {
-                            animation.setDirection(
-                                (animation.playDirection * -1) as AnimationDirection,
+                        events.emit('ended');
+
+                        if (playCountRef.current < iterations) {
+                            if (direction === 'reverseOnRepeat') {
+                                animation.setDirection(
+                                    (animation.playDirection * -1) as AnimationDirection,
+                                );
+                            }
+                            animation.goToAndPlay(
+                                animation[
+                                    animation.playDirection === 1 ? 'firstFrame' : 'totalFrames'
+                                ],
+                                true,
                             );
+                            events.emit('started');
                         }
-                        animation.goToAndPlay(
-                            animation[animation.playDirection === 1 ? 'firstFrame' : 'totalFrames'],
-                            true,
-                        );
-                    }
-                });
+                    }),
+                    animation.addEventListener('DOMLoaded', () => {
+                        setDataState(LottieDataState.OK);
+                    }),
+                    animation.addEventListener('data_failed', () => {
+                        setDataState(LottieDataState.ERROR);
+                    }),
+                    animation.addEventListener('enterFrame', ({ currentTime }) => {
+                        events.emit('frame', { currentFrame: currentTime });
+                    }),
+                ];
+
+                return () => {
+                    subscriptions.forEach((unsubscribe) => unsubscribe());
+                };
             }
 
             return noop;
-        }, [animation, iterations, reverseOnRepeat]);
+        }, [animation, direction, events, iterations]);
 
         return (
             <div className={cn(styles.component, className)} style={size}>
@@ -235,10 +180,9 @@ export const Lottie = forwardRef<LottieRef, LottieProps>(
                         [styles.show]: dataState === LottieDataState.OK,
                     })}
                 />
-                {dataState === LottieDataState.LOADING ||
-                    (dataState === LottieDataState.ERROR && (
-                        <div className={styles.placeholder}>{placeholder?.(dataState)}</div>
-                    ))}
+                {(dataState === LottieDataState.LOADING || dataState === LottieDataState.ERROR) && (
+                    <div className={styles.placeholder}>{placeholder?.(dataState)}</div>
+                )}
             </div>
         );
     },
--- packages/lottie/src/index.ts
+++ packages/lottie/src/index.ts
@@ -1,1 +1,2 @@
-export { Lottie, type LottieProps, LottieDataState } from './component';
+export { Lottie } from './component';
+export { type LottieProps, LottieDataState } from './types';
