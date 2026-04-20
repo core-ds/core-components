c62759a2f286d53685e5ad8ac060e962c1808a84
hextion <100ishundred@gmail.com>
2026-04-20 09:13:24 +0300

    refactor: backport types

--- packages/button/src/components/base-button-candidate/types.ts
+++ packages/button/src/components/base-button-candidate/types.ts
@@ -69,6 +69,9 @@ export interface BaseButtonCandidateOwnProps extends DisableAttributes {
      */
     Component?: ElementType<ComponentPropsWithoutRef<'a' | 'button'>, 'a' | 'button'>;
 
+    /**
+     * Кастомный компонент для контента
+     */
     Content?: ComponentType<BaseButtonContentProps>;
 }
 
