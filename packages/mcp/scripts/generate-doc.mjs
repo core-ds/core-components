7a0ecbf5eb9422c424e15cae2b877d7e7599829e
Mikhail Lukin <mishalykin@mail.ru>
2026-04-27 14:51:22 +0700

    feat: prepare mcp

--- packages/mcp/scripts/generate-doc.mjs
+++ packages/mcp/scripts/generate-doc.mjs
@@ -1,6 +1,9 @@
 import { withCustomConfig } from 'react-docgen-typescript';
 import { resolve } from 'node:path';
+import { isInheritedFromExternalTypes } from './is-inherited-from-external-types.mjs';
 
+// todo привести работу пропов к poc версии
+// todo в package json не должно быть оступов чтобы беречь контекст
 export function generateDoc(files) {
     const parser = withCustomConfig(
         resolve(process.cwd(), 'tsconfig.react-docgen-typescript.json'),
@@ -11,8 +14,18 @@ export function generateDoc(files) {
     const docsMap = new Map();
 
     docs.forEach((doc) => {
-        const { filePath, displayName, props } = doc;
+        const { filePath, displayName, props: _props } = doc;
         const packageName = filePath.split('packages/')[1].split('/')[0];
+
+        // if (packageName === 'accordion') {
+        //     console.warn(
+        //         Object.entries(props).filter(([key, prop]) => !isInheritedFromExternalTypes(prop)),
+        //     );
+        // }
+
+        const props = Object.fromEntries(
+            Object.entries(_props).filter(([_, prop]) => !isInheritedFromExternalTypes(prop)),
+        );
 
         if (!docsMap.has(packageName)) {
             docsMap.set(packageName, {
