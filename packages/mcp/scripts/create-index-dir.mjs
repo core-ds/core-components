6a43b96bd4a013bce2e4d340ea3598c640ab893f
Mikhail Lukin <mishalykin@mail.ru>
2026-04-27 17:54:17 +0700

    feat: prepare mcp

--- packages/mcp/scripts/create-index-dir.mjs
+++ packages/mcp/scripts/create-index-dir.mjs
@@ -9,7 +9,7 @@ const version = rootPackageJson.version;
 
 export function createIndexDir() {
     const indexDir = `v${version}`;
-    const versionDir = resolve(process.cwd(), 'packages/mcp/data', indexDir);
+    const versionDir = resolve(process.cwd(), 'packages/mcp/src/data', indexDir);
 
     rmSync(versionDir, { recursive: true, force: true });
     mkdirSync(versionDir, { recursive: true });
