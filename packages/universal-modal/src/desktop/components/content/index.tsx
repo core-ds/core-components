1ab1afed7361df8790501febf3f71a833455f7e7
Mikhail Lukin <mishalykin@mail.ru>
2026-05-08 16:21:06 +0700

    feat(universal-modal): lint

--- packages/universal-modal/src/desktop/components/content/index.tsx
+++ packages/universal-modal/src/desktop/components/content/index.tsx
@@ -4,6 +4,6 @@ import { BaseContent, type ContentProps } from '../../../components/base-content
 
 export type ContentDesktopProps = ContentProps;
 
-export const ContentDesktop: FC<ContentDesktopProps> = ({ className, ...restProps }) => {
-    return <BaseContent className={className} {...restProps} />;
-};
+export const ContentDesktop: FC<ContentDesktopProps> = ({ className, ...restProps }) => (
+    <BaseContent className={className} {...restProps} />
+);
