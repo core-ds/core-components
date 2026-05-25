0244dacf143320ebc4ac373434c4b8d1e0b0d716
Salavat Gizatov <100ishundred@gmail.com>
2026-05-25 08:03:28 +0300

    feat/new carousel (#2173)
    * feat(carousel): initial commit
    
    * refactor(carousel): better
    
    * refactor(carousel): migrated to `react-swipeable`
    
    * fix(carousel): invert translate sign
    
    * chore: better
    
    * fix: prevent user selection
    
    * fix: prevent user selection
    
    * fix: better
    
    * chore: storybook
    
    * refactor: swiper-like swipe handling
    
    * feat: added mouse wheel prop
    
    * chore: better
    
    * feat: added navigation
    
    * fix: css vars
    
    * docs: better storybook
    
    * chore: rename
    
    * fix: missed dependency
    
    * docs: demo
    
    * feat: layout
    
    * fix: css vars
    
    * fix: navigation
    
    * docs: better demo
    
    * fix: better mouse wheel
    
    * chore: yarn.lock
    
    * chore: version
    
    * chore: revert
    
    * chore: changeset
    
    * chore: demo
    
    * fix: package.json
    
    * fix: extend changeset
    
    * docs: dev
    
    * chore: file name
    
    * docs: jsdoc
    
    * feat: headless component

---
+++ packages/carousel/src/shared.ts
@@ -0,0 +1,3 @@
+export { AnimatedWrapper } from './components/animated-wrapper';
+export { CarouselLayout, type CarouselLayoutProps } from './components/layout';
+export { CarouselNavigation, type CarouselNavigationProps } from './components/navigation';
