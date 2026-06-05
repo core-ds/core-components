97c8c6f2e72f99d57e0a74769ec94f4f42aeb7da
hextion <100ishundred@gmail.com>
2026-06-05 20:01:24 +0300

    chore: jest report

--- jest.screenshots.config.mjs
+++ jest.screenshots.config.mjs
@@ -2,6 +2,7 @@
 
 /* eslint-disable import/no-extraneous-dependencies */
 import fse from 'fs-extra';
+import { defineConfig } from 'jest';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { createJsWithTsLegacyPreset, pathsToModuleNameMapper } from 'ts-jest';
@@ -12,10 +13,7 @@ const tsconfig = fse.readJsonSync(path.join(dirname, 'tsconfig.test.json'), { en
 
 const tsJestPreset = createJsWithTsLegacyPreset({ tsconfig: '<rootDir>/tsconfig.test.json' });
 
-/**
- * @type {import('ts-jest').JestConfigWithTsJest}
- */
-const config = {
+export default defineConfig({
     ...tsJestPreset,
     testEnvironment: 'node',
     setupFilesAfterEnv: ['<rootDir>/tools/jest/setupScreenshotsTests.ts'],
@@ -27,6 +25,8 @@ const config = {
     testMatch: ['**/*.screenshots.test.ts?(x)'],
     maxWorkers: 5,
     testTimeout: 200000,
-};
-
-export default config;
+    reporters:
+        process.env.CI === 'true'
+            ? [['github-actions', { silent: false }], 'summary']
+            : ['default'],
+});
