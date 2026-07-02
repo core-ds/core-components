d5f949b0739149b9dc60dacae8c571d50e01f6b3
Mikhail Lukin <mishalykin@mail.ru>
2026-07-02 14:07:59 +0700

    feat(bottom-sheet): entry points

--- packages/bottom-sheet/src/index.ts
+++ packages/bottom-sheet/src/index.ts
@@ -1,3 +1,3 @@
-export * from './component';
+export * from './Component';
 export type { BottomSheetProps, BottomSheetTitleAlign } from './types';
 export { CLOSE_OFFSET, HEADER_OFFSET } from './utils';
