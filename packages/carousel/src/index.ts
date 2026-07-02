7a80bce3e31272135c864e7acce9d3e77b9ac620
Mikhail Lukin <mishalykin@mail.ru>
2026-07-02 14:11:04 +0700

    feat(carousel): entry points

--- packages/carousel/src/index.ts
+++ packages/carousel/src/index.ts
@@ -1,4 +1,4 @@
-export { Carousel } from './component';
+export { Carousel } from './Component';
 export type { CarouselProps, HeadlessCarouselProps, CarouselContextValue } from './types';
 export { HeadlessCarousel } from './headless-component';
 export { CarouselContext, useCarouselContext } from './context';
