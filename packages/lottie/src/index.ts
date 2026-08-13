0be169563df670fc7adcf97a0d519b28de623726
hextion <100ishundred@gmail.com>
2026-08-13 15:34:38 +0300

    feat: useBounce hook

--- packages/lottie/src/index.ts
+++ packages/lottie/src/index.ts
@@ -1,2 +1,3 @@
 export { Lottie } from '@alfalab/core-components-lottie/component';
 export { type LottieProps, LottieDataState } from '@alfalab/core-components-lottie/types';
+export { useBounce } from '@alfalab/core-components-lottie/hooks';
