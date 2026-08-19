468c90afd927ede884da0826552d638df7e216ac
fulcanellee <45999900+fulcanellee@users.noreply.github.com>
2026-08-19 18:30:45 +0700

    feat/DS-18514_picker-button_dots (#2318)

--- packages/picker-button/src/utils/index.tsx
+++ packages/picker-button/src/utils/index.tsx
@@ -1,18 +1,27 @@
-import { type ComponentType, type SVGProps } from 'react';
+import React, { type ComponentType, type SVGProps } from 'react';
 
+import { Icon20Adapter } from '@alfalab/core-components-shared';
 import { ChevronDownCompactSIcon } from '@alfalab/icons-glyph/ChevronDownCompactSIcon';
 import { ChevronDownMIcon } from '@alfalab/icons-glyph/ChevronDownMIcon';
-import { MoreMIcon } from '@alfalab/icons-glyph/MoreMIcon';
-import { MoreSIcon } from '@alfalab/icons-glyph/MoreSIcon';
+import { DotsThreeHorizontalLine20Icon } from '@alfalab/icons-glyph-26/DotsThreeHorizontalLine20Icon';
+import { DotsThreeHorizontalLine24Icon } from '@alfalab/icons-glyph-26/DotsThreeHorizontalLine24Icon';
 
 import { type PickerButtonVariant } from '../types';
+
+const DotsThreeHorizontalLine20AdapterIcon = (props: SVGProps<SVGSVGElement>) => (
+    <Icon20Adapter icon={DotsThreeHorizontalLine20Icon} {...props} />
+);
 
 export const getCompactIcon = (size: string, Icon?: ComponentType<SVGProps<SVGSVGElement>>) => {
     if (Icon) {
         return Icon;
     }
 
-    return size === 'size-32' ? MoreSIcon : MoreMIcon;
+    if (size === 'size-32') {
+        return DotsThreeHorizontalLine20AdapterIcon;
+    }
+
+    return DotsThreeHorizontalLine24Icon;
 };
 
 export const getIcon = (
