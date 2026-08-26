12c1ba4e377fe76258065024b0184fd564de224b
Vadim Kalushko <vakalushko.job@yandex.ru>
2026-08-26 20:17:11 +0300

    feat(switch, radio, checkbox): добавлен 20 размер и пропса для компактного текста [DS-18095] (#2307)
    * feat(switch, radio, checkbox): добавлен 20 размер и пропса для компактного текста [DS-18095]
    
    * fix codeReview
    
    * test: updated screenshots
    
    * fix compact variant
    
    * test: updated screenshots
    
    * Update packages/switch/src/Component.tsx
    
    Co-authored-by: core-ds-bot <103739374+core-ds-bot@users.noreply.github.com>
    
    * add screenshots
    
    * test: updated screenshots
    
    * fix(radio): fix name codeReview [DS-18095]
    
    * fix(switch): name props
    
    * fix label
    
    * test: updated screenshots
    
    * fix radio reversed
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: core-ds-bot <103739374+core-ds-bot@users.noreply.github.com>

--- packages/checkbox/src/Component.test.tsx
+++ packages/checkbox/src/Component.test.tsx
@@ -107,6 +107,18 @@ describe('Checkbox', () => {
 
             expect(container.firstElementChild).toHaveClass('size-24');
         });
+
+        it('should set `compact` class for size 20', () => {
+            const { container } = render(<Checkbox size={20} compact={true} />);
+
+            expect(container.firstElementChild).toHaveClass('compact');
+        });
+
+        it('should not set `compact` class for size 24', () => {
+            const { container } = render(<Checkbox size={24} compact={true} />);
+
+            expect(container.firstElementChild).not.toHaveClass('compact');
+        });
     });
 
     describe('Attributes tests', () => {
