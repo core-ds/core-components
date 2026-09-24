cfae1f8c255394b801a34a298e19b99df06ffc5e
hextion <100ishundred@gmail.com>
2026-09-24 13:09:31 +0300

    fix: tokens

--- packages/tab-bar-island/src/component.tsx
+++ packages/tab-bar-island/src/component.tsx
@@ -39,7 +39,7 @@ export const TabBarIsland: FC<TabBarIslandProps> = ({
     return (
         <div
             className={cn(styles.component, className, {
-                [styles.margin]: items.length >= 4 || (items.length === 3 && trailingAddon),
+                [styles.offset]: items.length >= 4 || (items.length === 3 && trailingAddon),
             })}
         >
             <TabBarIslandTabList
